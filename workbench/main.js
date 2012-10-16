e9b0a0838d96ac6ff9fb3b8f3a524503fd51f7a2
Timothy Sweet <timothyswt@gmail.com>
2012-10-15 17:36:11 -0700

    updating workbench

--- workbench/main.js
+++ workbench/main.js
@@ -1,14 +1,8 @@
-function gridController($scope) {
-    $scope.myData = [{name: "Aislin", age: "4"},
-                 {name: "Desmond", age: "2"},
-                 {name: "Tim", age: "27"},
-                 {name: "Anna", age: "29"}];
-    $scope.btn_click = function(){
-        $scope.myData = [{name: "Aislin", age: 4},
-                         {name: "Desmond", age: 2},
-                         {name: "Tim", age: 27},
-                         {name: "Anna", age: 29},
-                         {name: "Ethan", age: 0}];
-    }
+function bodyController($scope) {
+    $scope.myData = [{name: "Moroni", age: 50},
+                     {name: "Tiancum", age: 43},
+                     {name: "Jacob", age: 27},
+                     {name: "Nephi", age: 29},
+                     {name: "Enos", age: 34}];
     $scope.gridOptions = { data : $scope.myData };
-}
+};
