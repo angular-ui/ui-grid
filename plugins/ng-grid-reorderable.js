720b47a150a89c322713fc53b49878387a20b5bc
Timothy Sweet <timothyswt@gmail.com>
2012-11-03 13:40:52 -0700

    fixing some comments

--- plugins/ng-grid-reorderable.js
+++ plugins/ng-grid-reorderable.js
@@ -2,50 +2,51 @@
     var self = this;
     self.$scope = null;
     self.myGrid = null;
-    // init method gets called during the ng-grid directive
+    // The init method gets called during the ng-grid directive execution.
     self.init = function (scope, grid) {
-        // init passes in the grid scope and the grid object which we will want to save for manipulation later
+        // The directive passes in the grid scope and the grid object which we will want to save for manipulation later.
         self.$scope = scope;
         self.myGrid = grid;
-        //in this example we assign grid events.
+        // In this example we want to assign grid events.
         self.assignEvents();
     };
     self.colToMove = undefined;
     self.colToReplace = undefined;
     self.assignEvents = function () {
-        // set the onmousedown event handler
+        // Here we set the onmousedown event handler to the header container.
         self.myGrid.$headerScroller.on('mousedown', self.onHeaderMouseDown);
     };
 
     self.onHeaderMouseDown = function (event) {
-        // get the closest header containr who's scope has the column we want
+        // Get the closest header container from where we clicked.
         var headerContainer = $(event.srcElement).closest('.ngHeaderSortColumn');
-        //get the scope
+        // Get the scope from the header container
         var headerScope = angular.element(headerContainer).scope();
-        //make sure we actually have a scope
+        // If we have a scope let's get the column associated with the header.
         if (headerScope) {
-            //save the column for later
+            // Save the column for later.
             self.colToMove = headerScope.col;
-            //set the onmouseup event last in case there was an issue getting here.
+            // Set the onmouseup event last in case there was an issue getting here.
             $(document).on('mouseup', self.onHeaderMouseUp);
         }
     };
-    
+
     self.onHeaderMouseUp = function (event) {
-        //turn off the mouseup event first so we don't blow things up in case of any issues
+        // Turn off mouseup event so things don't blow up, if there are any issues.
         $(document).off('mouseup');
         var headerContainer = $(event.srcElement).closest('.ngHeaderSortColumn');
         var headerScope = angular.element(headerContainer).scope();
         if (headerScope) {
             self.colToReplace = headerScope.col;
-            //splice in the columns
+            if (self.colToMove == self.colToReplace) return;
+            // Splice the columns
             self.$scope.columns.splice(self.colToMove.index, 1);
             self.$scope.columns.splice(self.colToReplace.index, 0, self.colToMove);
-            // fix all the indexes on the columns so if we reorder again things line up correctly.
+            // Fix all the indexes on the columns so if we reorder again the columns will line up correctly.
             angular.forEach(self.$scope.columns, function(col, i) {
                 col.index = i;
             });
-            // finally rebuild the CSS 
+            // Finally, rebuild the CSS styles.
             self.myGrid.cssBuilder.buildStyles();
         }
     };
