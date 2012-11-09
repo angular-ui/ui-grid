7c0f18e4b571039f9772d867b88df72d52dc1c86
jonricaurte <jonricaurte@gmail.com>
2012-11-08 16:42:11 -0800

    added more filter examples

--- workbench/main.js
+++ workbench/main.js
@@ -5,22 +5,22 @@ function userController($scope) {
     var self = this;
     $scope.mySelections = [];
     $scope.mySelections2 = [];
-    $scope.myData = [{name: "Moroni", age: 50, born: '350 A.D.', alive: true},
-                     {name: "Tiancum", age: 47, born: '350 A.D.', alive: false},
-                     {name: "Jacob", age: 27, born: '350 A.D.', alive: false},
-                     {name: "Nephi", age: 29, born: '350 A.D.', alive: false},
-                     {name: "Enos", age: 34, born: '350 A.D.', alive: false},
-                     {name: "Ether", age: 42, born: '350 A.D.', alive: false},
-                     {name: "Alma", age: 43, born: '350 A.D.', alive: true},
-                     {name: "Jared", age: 21, born: '350 A.D.', alive: true}];
+    $scope.myData = [{name: "Moroni", allowance: 50, birthday: 1351728000000 , alive: true},
+                     {name: "Tiancum", allowance: 47, birthday: 1351728000000 , alive: false},
+                     {name: "Jacob", allowance: 27, birthday: 1351728000000 , alive: false},
+                     {name: "Nephi", allowance: 29, birthday: 1351728000000 , alive: false},
+                     {name: "Enos", allowance: 34, birthday: 1351728000000 , alive: false},
+                     {name: "Ether", allowance: 42, birthday: 1288323623006 , alive: false},
+                     {name: "Alma", allowance: 43, birthday: 1288323623006 , alive: true},
+                     {name: "Jared", allowance: 21, birthday: 1288323623006 , alive: true}];
     $scope.gridOptions = {
         data: 'myData',
         selectedItems: $scope.mySelections,
         multiSelect: true,
         plugins: [new ngGridReorderable()],
         columnDefs: [{ field: 'name', displayName: 'Very Long Name Title', width: 200, cellTemplate: '<div><input ng-model="row.entity[col.field]"/></div>'},
-                     { field: 'age', width: 100, cellTemplate: '<div ng-class="{red: row.entity[col.field] > 30}"><div class="ngCellText">{{row.entity[col.field] | currency}}</div></div>'},
-                     { field: 'born', width: 100 },
+                     { field: 'allowance', width: 100, cellTemplate: '<div ng-class="{red: row.entity[col.field] > 30}"><div class="ngCellText">{{row.entity[col.field] | currency}}</div></div>'},
+                     { field: 'birthday', width: 100, cellTemplate: '<div class="ngCellText">{{row.entity[col.field] | date}}</div>' },
                      { field: 'alive', width: 100, cellTemplate: '<div ng-class="{green: row.entity[col.field], red: !row.entity[col.field] }"><div class="ngCellText">{{row.entity[col.field] | checkmark}}</div></div>' }]
     };
     $scope.myData2 = [{name: "MoroniMoroniMoroniMoroniMoroniMoroniMoroni", age: 50, born: '350 A.D.'},
