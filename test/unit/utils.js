6b483885588f0b24708e4d40a717e534808e9635
Timothy Sweet <timothyswt@gmail.com>
2012-11-20 09:54:51 -0800

    getting set up for tests

---
+++ test/unit/utils.js
@@ -0,0 +1,16 @@
+﻿describe('Utils Test', function () {
+
+    describe('evalProperty', function () {
+        var res;
+        beforeEach(function() {
+            var obj = { foo: { bar: { hello: { world: "foundme" } } } };
+            res = ng.utils.evalProperty(obj, "foo.bar.hello.world");
+        });
+        
+        it('should find the right property given a heirarchy', function () {
+            expect(res).toEqual("foundme");
+        });
+        
+    });
+    
+});
