1d82dcd5991e45926ae694bd2e6f1f35cd6571d2
Timothy Sweet <timothyswt@gmail.com>
2012-11-19 17:26:22 -0800

    Incrementing version # and updating readme

--- ng-grid-1.2.0.debug.js
+++ ng-grid-1.2.0.debug.js
@@ -2,7 +2,7 @@
 * ng-grid JavaScript Library
 * Authors: https://github.com/Crash8308/ng-grid/blob/master/README.md
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-* Compiled At: 11/19/2012 17:19:05
+* Compiled At: 11/19/2012 17:23:54
 ***********************************************/
 
 (function(window, undefined){
--- workbench/main.js
+++ workbench/main.js
@@ -39,8 +39,6 @@ function userController($scope) {
                      { field: 'birthday', width: '120px', cellFilter: 'date' },
                      { field: 'paid', width: '*',  cellFilter: 'checkmark' }]
     };
-    
-
     $scope.myData2 = [{ 'Sku': 'C-2820164', 'Vendor': 'NEWB', 'SeasonCode': null, 'Mfg_Id': '573-9880954', 'UPC': '822860449228' },
                       { 'Sku': 'J-8555462', 'Vendor': 'NIKE', 'SeasonCode': '', 'Mfg_Id': '780-8855467', 'UPC': '043208523549' },
                       { 'Sku': 'K-5312708', 'Vendor': 'REEB', 'SeasonCode': '1293', 'Mfg_Id': '355-6906843', 'UPC': '229487568922' },
