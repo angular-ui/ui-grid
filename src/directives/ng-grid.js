de38ea6ccbe8d14b8421c533978abb706b0957b1
Andy Joslin <andytjoslin@gmail.com>
2012-11-19 20:55:29 -0500

    Make ng-grid options use .

--- src/directives/ng-grid.js
+++ src/directives/ng-grid.js
@@ -5,7 +5,7 @@
             return {
                 pre: function ($scope, iElement, iAttrs) {
                     var $element = $(iElement);
-                    var options = $scope[iAttrs.ngGrid];
+                    var options = $scope.$eval(iAttrs.ngGrid);
                     var gridDim = new ng.Dimension({ outerHeight: $($element).height(), outerWidth: $($element).width() });
                     var grid = new ng.Grid($scope, options, gridDim, SortService);
                     var htmlText = ng.defaultGridTemplate(grid.config);
