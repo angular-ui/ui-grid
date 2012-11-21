ba9cf77f05123426276fdab9f790ad1b154a16fe
Timothy Sweet <timothyswt@gmail.com>
2012-11-21 11:13:10 -0800

    adding server-side filtering example

--- javascripts/mainssp.js
+++ javascripts/mainssp.js
@@ -4,21 +4,34 @@
 function userController($scope) {
     var self = this;
     $scope.mySelections = [];
+    $scope.filterOptions = {
+        filterText: "",
+        useExternalFilter: true
+    },
     $scope.pagingOptions = {
         pageSizes: [250, 500, 1000], //page Sizes
         pageSize: 250, //Size of Paging data
         totalServerItems: 0, //how many items are on the server (for paging)
         currentPage: 1 //what page they are currently on
     };
-    self.getPagedDataAsync = function (pageSize, page) {
-        var data = largeLoad();
-        var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
-        $scope.myData = pagedData;
-        $scope.pagingOptions.totalServerItems = data.length;
+    self.getPagedDataAsync = function (pageSize, page, searchText) {
+        setTimeout(function() {
+            var ft = searchText.toLowerCase();
+            var data = largeLoad().filter(function(item) {
+                return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
+            });
+            var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+            $scope.myData = pagedData;
+            $scope.pagingOptions.totalServerItems = data.length;
+        }, 100);
     };
     $scope.$watch('pagingOptions', function () {
-        self.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+        self.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
     }, true);
+    $scope.$watch('filterOptions', function () {
+        self.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+    }, true);
+    
     $scope.myData = self.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
     $scope.gridOptions = {
 		data: 'myData',
@@ -31,6 +44,7 @@ function userController($scope) {
         showColumnMenu: true,
         enablePaging: true,
         pagingOptions: $scope.pagingOptions,
+        filterOptions: $scope.filterOptions,
         columnDefs: [{ field: 'name', displayName: 'Very Long Name Title'},
                      { field: 'allowance', width: 'auto', aggLabelFilter: 'currency', cellTemplate: '<div ng-class="{red: row.entity[col.field] > 30}"><div class="ngCellText">{{row.entity[col.field] | currency}}</div></div>' },
                      { field: 'birthday', width: '120px', cellFilter: 'date' },
