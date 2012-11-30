0d81f3c542cb370b9a0d734df1511030b6edfa42
jonricaurte <jonricaurte@gmail.com>
2012-11-30 15:18:45 -0800

    Added placeholder page for examples. Added plunker to getting started.

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.services', 'myApp.controllers', 'ngGrid']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/applications', {templateUrl: 'partials/overviewPage.html', controller: 'OverviewPageCtrl'});
+    $routeProvider.when('/overview', {templateUrl: 'partials/overviewPage.html', controller: 'OverviewPageCtrl'});
     $routeProvider.when('/gettingstarted', {templateUrl: 'partials/gettingStartedPage.html', controller: 'GettingStartedPageCtrl'});
-    $routeProvider.when('/examples', {templateUrl: 'partials/gettingStartedPage.html', controller: 'ExamplesPageCtrl'});
-    $routeProvider.otherwise({redirectTo: '/applications'});
+    $routeProvider.when('/examples', {templateUrl: 'partials/examplesPage.html', controller: 'ExamplesPageCtrl'});
+    $routeProvider.otherwise({redirectTo: '/overview'});
   }]);
