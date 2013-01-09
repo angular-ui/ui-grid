1170a0f2b1ef6c48f52db4b01d9cafc7e56756eb
jonricaurte <jonricaurte@gmail.com>
2013-01-09 11:13:55 -0800

    updated ng grid debug

--- build/ng-grid.debug.js
+++ build/ng-grid.debug.js
@@ -2,7 +2,7 @@
 * ng-grid JavaScript Library
 * Authors: https://github.com/angular-ui/ng-grid/blob/master/README.md
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-* Compiled At: 01/09/2013 10:50:33
+* Compiled At: 01/09/2013 11:13:12
 ***********************************************/
 
 (function(window) {
@@ -2449,59 +2449,5 @@ window.ngGrid.i18n['en'] = {
     ngPagerNextTitle: 'Next Page',
     ngPagerPrevTitle: 'Previous Page',
     ngPagerLastTitle: 'Last Page'
-};
-
-/***********************************************
-* LANGUAGE: fr.js
-***********************************************/
-window.ngGrid.i18n['fr'] = {
-    ngAggregateLabel: 'articles',
-    ngGroupPanelDescription: 'Faites glisser un en-tête de colonne ici et déposez-le vers un groupe par cette colonne.',
-    ngSearchPlaceHolder: 'Recherche...',
-    ngMenuText: 'Choisir des colonnes:',
-    ngShowingItemsLabel: 'Articles Affichage des:',
-    ngTotalItemsLabel: 'Nombre total d\'articles:',
-    ngSelectedItemsLabel: 'Éléments Articles:',
-    ngPageSizeLabel: 'Taille de page:',
-    ngPagerFirstTitle: 'Première page',
-    ngPagerNextTitle: 'Page Suivante',
-    ngPagerPrevTitle: 'Page précédente',
-    ngPagerLastTitle: 'Dernière page'
-};
-
-/***********************************************
-* LANGUAGE: ge.js
-***********************************************/
-window.ngGrid.i18n['ge'] = {
-    ngAggregateLabel: 'artikel',
-    ngGroupPanelDescription: 'Ziehen Sie eine Spaltenüberschrift hier und legen Sie es der Gruppe nach dieser Spalte.',
-    ngSearchPlaceHolder: 'Suche...',
-    ngMenuText: 'Spalten auswählen:',
-    ngShowingItemsLabel: 'Zeige Artikel:',
-    ngTotalItemsLabel: 'Meiste Artikel:',
-    ngSelectedItemsLabel: 'Ausgewählte Artikel:',
-    ngPageSizeLabel: 'Größe Seite:',
-    ngPagerFirstTitle: 'Erste Page',
-    ngPagerNextTitle: 'Nächste Page',
-    ngPagerPrevTitle: 'Vorherige Page',
-    ngPagerLastTitle: 'Letzte Page'
-};
-
-/***********************************************
-* LANGUAGE: sp.js
-***********************************************/
-window.ngGrid.i18n['sp'] = {
-    ngAggregateLabel: 'Artículos',
-    ngGroupPanelDescription: 'Arrastre un encabezado de columna aquí y soltarlo para agrupar por esa columna.',
-    ngSearchPlaceHolder: 'Buscar...',
-    ngMenuText: 'Elegir columnas:',
-    ngShowingItemsLabel: 'Artículos Mostrando:',
-    ngTotalItemsLabel: 'Artículos Totales:',
-    ngSelectedItemsLabel: 'Artículos Seleccionados:',
-    ngPageSizeLabel: 'Tamaño de Página:',
-    ngPagerFirstTitle: 'Primera Página',
-    ngPagerNextTitle: 'Página Siguiente',
-    ngPagerPrevTitle: 'Página Anterior',
-    ngPagerLastTitle: 'Última Página'
 };
 }(window));
--- ng-grid-1.6.1.debug.js
+++ ng-grid-1.6.1.debug.js
@@ -2,7 +2,7 @@
 * ng-grid JavaScript Library
 * Authors: https://github.com/angular-ui/ng-grid/blob/master/README.md
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-* Compiled At: 01/09/2013 10:43:30
+* Compiled At: 01/09/2013 11:13:12
 ***********************************************/
 
 (function(window) {
@@ -2179,7 +2179,7 @@ ng.SelectionService = function(grid) {
 				}
 				self.setSelection(self.lastClickedRow, false);
 				if(self.lastClickedRow.entity == rowItem.entity){ 
-					self.lastClickedRow = undefined; //deselected row
+					self.lastClickedRow = undefined; //deselect row
 					return true;
 				}
 				self.setSelection(rowItem, grid.config.keepLastSelected ? true : !rowItem.selected);
