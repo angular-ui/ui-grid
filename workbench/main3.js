2bcc16abe83195044d7ec3e5395a4b93622115d6
jonricaurte <jonricaurte@gmail.com>
2013-01-19 04:36:57 -0800

    Fix for late bound data with initial grouping.

--- workbench/main3.js
+++ workbench/main3.js
@@ -4,11 +4,7 @@ function userController($scope) {
         { field: 'SeasonCode', displayName: 'My SeasonCode' },
         { field: 'Mfg_Id', displayName: 'Manufacturer ID' },
         { field: 'UPC', displayName: 'Bar Code' }];
-    
-    $scope.myData = [{ 'Sku': 'C-2820164', 'Vendor': 'NEWB', 'SeasonCode': 0, 'Mfg_Id': '573-9880954', 'UPC': '822860449228' },
-                      { 'Sku': 'J-8555462', 'Vendor': 'NIKE', 'SeasonCode': 0, 'Mfg_Id': '780-8855467', 'UPC': '043208523549' },
-                      { 'Sku': 'K-5312708', 'Vendor': 'REEB', 'SeasonCode': 04, 'Mfg_Id': '355-6906843', 'UPC': '229487568922' }];
-					  
+		
 	$scope.selections = [];
 	
 	$scope.gridOptions = {
@@ -19,7 +15,13 @@ function userController($scope) {
         enableRowReordering: false,
         showGroupPanel: true,
         columnDefs: 'myDefs',
-		groups: ['Sku']
+		groups: ['Sku', 'SeasonCode', 'Vendor']
     };
 	
+	$scope.getData = function(){
+		$scope.myData = [{ 'Sku': 'C-2820164', 'Vendor': 'NEWB', 'SeasonCode': 0, 'Mfg_Id': '573-9880954', 'UPC': '822860449228' },
+						  { 'Sku': 'J-8555462', 'Vendor': 'NIKE', 'SeasonCode': 0, 'Mfg_Id': '780-8855467', 'UPC': '043208523549' },
+						  { 'Sku': 'K-5312708', 'Vendor': 'REEB', 'SeasonCode': 04, 'Mfg_Id': '355-6906843', 'UPC': '229487568922' }];
+					  
+	};
 }
