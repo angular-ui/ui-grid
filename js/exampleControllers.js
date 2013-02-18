467c65fd4d58d9c7b9987c5fd5665f2863a8cdd0
Timothy Sweet <timothyswt@gmail.com>
2013-02-18 15:00:19 -0800

    adding column pinning example

--- js/exampleControllers.js
+++ js/exampleControllers.js
@@ -311,4 +311,37 @@ angular.module('myApp.exampleControllers', [])
 			}
 		});
 	};
+}])
+
+.controller('ColumnPinningExampleCtrl', ['$scope', function ($scope) {
+    $scope.basicTabs = tabs;
+    $scope.selectedBasicTab = $scope.basicTabs[0];
+    $scope.switchTab = function (tab) {
+        $scope.selectedBasicTab = $scope.basicTabs[tab];
+    };
+    $scope.link = function () { return "partials/exampleDefinitions/columnPinning/columnPinning" + $scope.selectedBasicTab.title + '.html'; };
+    
+    $scope.myData = [{ name: "Moroni", age: 50, birthday: "Oct 28, 1970", salary: "60,000" },
+                     { name: "Tiancum", age: 43, birthday: "Feb 12, 1985", salary: "70,000" },
+                     { name: "Jacob", age: 27, birthday: "Aug 23, 1983", salary: "50,000" },
+                     { name: "Nephi", age: 29, birthday: "May 31, 2010", salary: "40,000" },
+                     { name: "Enos", age: 34, birthday: "Aug 3, 2008", salary: "30,000" },
+                     { name: "Moroni", age: 50, birthday: "Oct 28, 1970", salary: "60,000" },
+                     { name: "Tiancum", age: 43, birthday: "Feb 12, 1985", salary: "70,000" },
+                     { name: "Jacob", age: 27, birthday: "Aug 23, 1983", salary: "40,000" },
+                     { name: "Nephi", age: 29, birthday: "May 31, 2010", salary: "50,000" },
+                     { name: "Enos", age: 34, birthday: "Aug 3, 2008", salary: "30,000" },
+                     { name: "Moroni", age: 50, birthday: "Oct 28, 1970", salary: "60,000" },
+                     { name: "Tiancum", age: 43, birthday: "Feb 12, 1985", salary: "70,000" },
+                     { name: "Jacob", age: 27, birthday: "Aug 23, 1983", salary: "40,000" },
+                     { name: "Nephi", age: 29, birthday: "May 31, 2010", salary: "50,000" },
+                     { name: "Enos", age: 34, birthday: "Aug 3, 2008", salary: "30,000" }];
+    $scope.gridOptions = {
+        data: 'myData',
+        enablePinning: true,
+        columnDefs: [{ field: "name", width: 120, pinned: true },
+                    { field: "age", width: 120 },
+                    { field: "birthday", width: 120 },
+                    { field: "salary", width: 120 }]
+    };
 }]);
