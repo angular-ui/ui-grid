2fb4e140676105177aff3ada0af741d7c5a07c7a
Tim Sweet <timothyswt@gmail.com>
2013-03-07 14:06:08 -0800

    forgot closure

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -3,15 +3,16 @@
 /* jasmine specs for services go here */
 
 describe('Utility Service', function() {
-    describe('evalProperty should find the right property given a heirarchy', function () {
+    describe('evalProperty should find the right property given a heirarchy', function() {
         var service;
         beforeEach(module('ngGrid.services'));
-        beforeEach(inject(function ($utilityService) {
+        beforeEach(inject(function($utilityService) {
             service = $utilityService;
         }));
-		
-        it('returns "foundme"', function () {
+
+        it('returns "foundme"', function() {
             var obj = { foo: { bar: { hello: { world: "foundme" } } } };
             expect(service.evalProperty(obj, "foo.bar.hello.world")).toEqual("foundme");
         });
+    });
 });
