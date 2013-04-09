297639b74accf8f57c22e3959e38cd47dd26d1f1
Tim Sweet <timothyswt@gmail.com>
2013-04-08 17:40:51 -0700

    updating to 2.0.4

--- build/ng-grid.js
+++ build/ng-grid.js
@@ -2,7 +2,7 @@
 * ng-grid JavaScript Library
 * Authors: https://github.com/angular-ui/ng-grid/blob/master/README.md 
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-* Compiled At: 04/08/2013 17:12
+* Compiled At: 04/08/2013 17:19
 ***********************************************/
 (function(window, $) {
 'use strict';
