463450844be0244342a47c8f2b34d0901ba9dc85
Tim Sweet <timothyswt@gmail.com>
2013-04-09 12:49:40 -0700

    Update ng-grid-flexible-height.js
    reverting the change that was made earlier.

--- plugins/ng-grid-flexible-height.js
+++ plugins/ng-grid-flexible-height.js
@@ -1,16 +1,37 @@
-ngGridFlexibleHeightPlugin = function() {
+ngGridFlexibleHeightPlugin = function (opts) {
     var self = this;
     self.grid = null;
     self.scope = null;
-    self.init = function(scope, grid, services) {
+    self.init = function (scope, grid) {
         self.grid = grid;
         self.scope = scope;
-        self.services = services;
-        var recalcHeightForData = function () { setTimeout(innerRecalcForData, 1); } ;
+        var recalcHeightForData = function () { setTimeout(innerRecalcForData, 1); };
         var innerRecalcForData = function () {
-            self.grid.$viewport.css('height', (self.grid.$canvas.height() + 5) + 'px');
-            self.services.DomUtilityService.RebuildGrid(self.scope, self.grid);
+            var gridId = self.grid.gridId;
+            var footerPanelSel = '.' + gridId + ' .ngFooterPanel';
+            var extraHeight = self.grid.$topPanel.height() + $(footerPanelSel).height();
+            var naturalHeight = self.grid.$canvas.height() + 1;
+            if (scope.baseViewportHeight == null || scope.baseViewportHeight == 0) {
+                scope.baseViewportHeight = self.grid.$viewport.height();
+            }
+            if (scope.baseViewportHeight > naturalHeight) {
+                if (opts != null) {
+                    if (opts.minHeight != null && (naturalHeight + extraHeight) < opts.minHeight) {
+                        naturalHeight = opts.minHeight - extraHeight - 2;
+                    }
+                }
+                self.grid.$viewport.css('height', (naturalHeight + 2) + 'px');
+                self.grid.$root.css('height', (naturalHeight + extraHeight + 2) + 'px');
+            }
+            self.grid.refreshDomSizes();
         };
-        scope.$watch (grid.config.data, recalcHeightForData);
+        scope.catHashKeys = function () {
+            var hash = '',
+                idx;
+            for (idx in scope.renderedRows) { hash += scope.renderedRows[idx].$$hashKey; }
+            return hash;
+        };
+        scope.$watch('catHashKeys()', innerRecalcForData);
+        scope.$watch(grid.config.data, recalcHeightForData);
     };
 };
