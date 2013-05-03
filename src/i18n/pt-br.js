ab0f2073994d158e26ab1771180e55c891c072f9
Brian Hann <emailc0bra@gmail.com>
2013-05-03 11:49:52 -0500

    Adding pt-br.js (Brazilian Portugeuse) to i18n
    Fixes #371
    
    Thanks to @dipold for the translations and pull request.

---
+++ src/i18n/pt-br.js
@@ -0,0 +1,14 @@
+window.ngGrid.i18n['pt-br'] = {
+    ngAggregateLabel: 'items',
+    ngGroupPanelDescription: 'Arraste e solte uma coluna aqui para agrupar por essa coluna',
+    ngSearchPlaceHolder: 'Procurar...',
+    ngMenuText: 'Selecione as colunas:',
+    ngShowingItemsLabel: 'Mostrando os Items:',
+    ngTotalItemsLabel: 'Total de Items:',
+    ngSelectedItemsLabel: 'Items Selecionados:',
+    ngPageSizeLabel: 'Tamanho da Página:',
+    ngPagerFirstTitle: 'Primeira Página',
+    ngPagerNextTitle: 'Próxima Página',
+    ngPagerPrevTitle: 'Página Anterior',
+    ngPagerLastTitle: 'Última Página'
+};
