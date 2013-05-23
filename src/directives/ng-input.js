fb76a3829e24606829babbf8a4c869910aa8aa64
Shane Walters <swalters@singletreetech.com>
2013-05-23 09:09:54 -0500

    Issue #442 - Attempt at fixing Editing in 2.0.6 branch
    -Removed the $eval on COL_FIELD placeholder
    -Simplified ng-input to only be concerned with hiding keystrokes and
    using ESC to backout edit
    -Use ng-model in EditableCellTemplate to do actual binding

--- src/directives/ng-input.js
+++ src/directives/ng-input.js
@@ -1,27 +1,12 @@
 ﻿ngGridDirectives.directive('ngInput',['$parse', function($parse) {
   return function ($scope, elm, attrs) {
-    
-    // Strip the $eval off of the ng-input tag, we want WHERE to put the edited values, not the actual edited/displayed value itself
-    var inputter = attrs.ngInput.replace(/.+?\((.+?)\)/, '$1');
-
-    var evaled = $scope.$eval(inputter);
-    var getter = $parse(evaled);
-		var setter = getter.assign;
-		var oldCellValue = getter($scope);
-
-		elm.val(oldCellValue);
-
-    elm.bind('keyup', function() {
-        var newVal = elm.val();
-        // dump(newVal);
-        if (!$scope.$root.$$phase) {
-          $scope.$apply(function(){
-            setter($scope, newVal);
-          });
-        }
-    });
-
-		elm.bind('keydown', function(evt){
+    var field = attrs.ngInput;
+    var getter = $parse(field);
+    var setter = getter.assign;
+      
+    var oldCellValue = $scope.$eval(field);
+     
+    elm.bind('keydown', function(evt){
 			switch(evt.keyCode){
 				case 37: // Left arrow
 				case 38: // Up arrow
@@ -31,19 +16,14 @@
 					break;
 				case 27: // Esc (reset to old value)
 					if (!$scope.$root.$$phase) {
-						$scope.$apply(function(){
-							setter($scope, oldCellValue);
+						$scope.$apply(function() {
+						    setter($scope, oldCellValue);
 							elm.val(oldCellValue);
 							elm.blur();
 						});
 					}
-        case 13: // Enter (apply new value)
-          if (!$scope.$root.$$phase) {
-            $scope.$apply(function(){
-              setter($scope, elm.val());
-              elm.blur();
-            });
-          }
+			    case 13: // Enter (Leave Field)
+			        elm.blur();
 				default:
 					break;
 			}
