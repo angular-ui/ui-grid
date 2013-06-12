683654469d0171a2348ec348cf41d9a69602d10c
Andreas Kemkes <a5sk4s@yahoo.com>
2013-06-12 16:25:39 -0700

    rebuild

--- build/ng-grid.debug.js
+++ build/ng-grid.debug.js
@@ -2,7 +2,7 @@
 * ng-grid JavaScript Library
 * Authors: https://github.com/angular-ui/ng-grid/blob/master/README.md 
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-* Compiled At: 06/10/2013 20:31
+* Compiled At: 06/12/2013 16:24
 ***********************************************/
 (function(window, $) {
 'use strict';
