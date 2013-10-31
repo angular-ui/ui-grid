5dc27cd9850799cc5a04e7c2561e514ab22cd070
John Nadratowski <john.nadratowski@gmail.com>
2013-10-31 15:27:06 -0400

    Added InterpolateUpdateService for easy updating of the interpolation symbols in templates

---
+++ src/services/InterpolateUpdateService.js
@@ -0,0 +1,33 @@
+﻿angular.module("ngGrid.services").service("$InterpolateUpdateService", ['$templateCache', '$interpolate', function($templateCache, $interpolate){
+    this.changeGridInterpolate = function() {
+
+        var templates = [
+            "aggregateTemplate.html",
+            "rowTemplate.html",
+            "cellTemplate.html",
+            "cellEditTemplate.html",
+            "checkboxCellTemplate.html",
+            "editableCellTemplate.html",
+            "headerRowTemplate.html",
+            "headerCellTemplate.html",
+            "checkboxHeaderTemplate.html",
+            "gridTemplate.html",
+            "footerTemplate.html",
+            "menuTemplate.html"
+        ];
+
+        var start = $interpolate.startSymbol();
+        var end = $interpolate.endSymbol();
+        for (var i = 0; i < templates.length; i++){
+            var template = templates[i];
+            var cur_template = $templateCache.get(template);
+            if (start != "}}"){
+                cur_template = cur_template.replace(/\{\{/g, start);
+            }
+            if (end != "}}"){
+                cur_template = cur_template.replace(/\}\}/g, end);
+            }
+            $templateCache.put(template, cur_template);
+        }
+    }
+}]);
