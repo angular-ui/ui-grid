65302c2faec945a197a0326d21ee24e276b14299
Shane Walters <swalters@singletreetech.com>
2013-12-09 11:53:23 -0600

    Allow calculated, declarative, or imperative columnDefs and gridOptions

--- src/js/directives/ui-grid.js
+++ src/js/directives/ui-grid.js
@@ -9,15 +9,32 @@ app.directive('uiGrid', ['GridUtil', function(GridUtil) {
     // transclude: true,
     scope: {
       uiGrid: '=',
-      tableClass: '@uiGridTableClass'
+      tableClass: '@uiGridTableClass',
+      options: '@uiGridOptions'
     },
-    link: function(scope, elm, attrs) {
-      
-    },
-    controller: ['$scope', function($scope) {
-      this.gridData = $scope.uiGrid;
+    link: function ($scope, elm, attrs, controller) {
+       
+     
 
-      this.columns = GridUtil.getColumnsFromData($scope.uiGrid);
+    },
+    controller: ['$scope','$element','$attrs', function($scope, $element, $attrs) {
+        this.gridData = $scope.uiGrid;
+
+        $scope.gridOptions = {};
+        
+        //use parent scope options if specified
+        if ($scope.options) {
+            if (!$scope.$parent[$scope.options]) {
+                throw new Error($scope.options + ' was not defined in parent scope');
+            }
+            $scope.gridOptions = $scope.$parent[$scope.options];
+        }
+
+        //use gridOptions.columns or ui-grid-columns attribute json or get the columns from the data
+        this.columns = $scope.gridOptions.columnDefs || $scope.$eval($attrs.uiGridColumns) || GridUtil.getColumnsFromData($scope.uiGrid);
+
+        $scope.gridOptions.columnDefs = $scope.gridOptions.columnDefs || this.columns;
+
     }]
   };
 }]);
