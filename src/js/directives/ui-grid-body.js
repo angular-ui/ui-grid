f248a1565f22890a7d2e314dd8ac609cea4e97cd
Brian Hann <emailc0bra@gmail.com>
2013-12-09 09:33:56 -0600

    WIP: making use of template conditional

--- src/js/directives/ui-grid-body.js
+++ src/js/directives/ui-grid-body.js
@@ -3,7 +3,7 @@
 
 var app = angular.module('ui.grid.body', []);
 
-app.directive('uiGridBody', ['GridUtil', function(GridUtil) {
+app.directive('uiGridBody', ['$log', 'GridUtil', function($log, GridUtil) {
   return {
     templateUrl: 'ui-grid/ui-grid-body',
     replace: true,
@@ -12,6 +12,10 @@ app.directive('uiGridBody', ['GridUtil', function(GridUtil) {
       tableClass: '=uiGridTableClass'
     },
     link: function(scope, elm, attrs, uiGridCtrl) {
+      if (!uiGridCtrl) {
+        $log.warn('[ui-grid-body] uiGridCtrl is undefined!');
+      }
+
       if (uiGridCtrl && typeof(uiGridCtrl.columns) !== 'undefined' && uiGridCtrl.columns) {
         scope.columns = uiGridCtrl.columns;
       }
--- src/js/directives/ui-grid.js
+++ src/js/directives/ui-grid.js
@@ -3,7 +3,13 @@
 
 var app = angular.module('ui.grid', ['ui.grid.header', 'ui.grid.body']);
 
-app.directive('uiGrid', ['GridUtil', function(GridUtil) {
+app.directive('uiGrid', ['$compile', '$templateCache', '$log', 'GridUtil', function($compile, $templateCache, $log, GridUtil) {
+  function linkFn(scope, elm, attrs, controller) {
+    console.log(scope.tableClass);
+
+    
+  }
+
   return {
     templateUrl: 'ui-grid/ui-grid',
     // transclude: true,
@@ -11,11 +17,27 @@ app.directive('uiGrid', ['GridUtil', function(GridUtil) {
       uiGrid: '=',
       tableClass: '@uiGridTableClass'
     },
-    link: function(scope, elm, attrs) {
-      
-    },
+    // compile: function (elm, attrs) {
+    //   // If the contents of the grid element are empty, use the default grid template
+    //   var newContent;
+    //   if (/^\s*$/.test(elm.html())) {
+    //     newContent = $templateCache.get('ui-grid/ui-grid');
+    //   }
+
+    //   var linker = function (scope, elm, attrs) {
+    //     if (newContent) {
+    //       elm.replaceWith($compile(newContent)(scope));
+    //     }
+    //   };
+
+    //   return {
+    //     pre: linker,
+    //     post: linkFn
+    //   };
+    // },
     controller: ['$scope', function($scope) {
       this.gridData = $scope.uiGrid;
+      // $log.debug('grid Data', this.gridData);
 
       this.columns = GridUtil.getColumnsFromData($scope.uiGrid);
     }]
