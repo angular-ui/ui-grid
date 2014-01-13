8e9244b784dda2d7c9a40a6670a4a3fea325026e
Brian Hann <emailc0bra@gmail.com>
2014-01-13 10:03:39 -0600

    Comment out skeleton code

--- src/js/edit/uiGridEdit.js
+++ src/js/edit/uiGridEdit.js
@@ -1,27 +1,27 @@
 (function(){
   'use strict';
 
-  var app = angular.module('ui.grid.header', ['ui.grid']);
+  // var app = angular.module('ui.grid.header', ['ui.grid']);
 
-  app.directive('uiGridHeader', ['$log', '$templateCache', '$compile', 'gridUtil', function($log, $templateCache, $compile, gridUtil) {
-    return {
-      restrict: 'EA',
-      templateUrl: 'ui-grid/ui-grid-header',
-      replace: true,
-      // priority: 1000,
-      require: '?^uiGrid',
-      scope: false,
-      link: function ($scope, $elm, $attrs, uiGridCtrl) {
-        if (uiGridCtrl === undefined) {
-          throw new Error('[ui-grid-header] uiGridCtrl is undefined!');
-        }
-        $log.debug('ui-grid-header link');
+  // app.directive('uiGridHeader', ['$log', '$templateCache', '$compile', 'gridUtil', function($log, $templateCache, $compile, gridUtil) {
+  //   return {
+  //     restrict: 'EA',
+  //     templateUrl: 'ui-grid/ui-grid-header',
+  //     replace: true,
+  //     // priority: 1000,
+  //     require: '?^uiGrid',
+  //     scope: false,
+  //     link: function ($scope, $elm, $attrs, uiGridCtrl) {
+  //       if (uiGridCtrl === undefined) {
+  //         throw new Error('[ui-grid-header] uiGridCtrl is undefined!');
+  //       }
+  //       $log.debug('ui-grid-header link');
 
-        if (uiGridCtrl) {
-          uiGridCtrl.header = $elm;
-        }
-      }
-    };
-  }]);
+  //       if (uiGridCtrl) {
+  //         uiGridCtrl.header = $elm;
+  //       }
+  //     }
+  //   };
+  // }]);
 
 })();
