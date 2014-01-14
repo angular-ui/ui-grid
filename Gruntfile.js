0b8113cf9e3e788110c7587c976f827a97b1ea5f
Brian Hann <emailc0bra@gmail.com>
2014-01-14 11:11:14 -0600

    Switch over to googleapis CDN; code.angularjs.org is throwing errors

--- Gruntfile.js
+++ Gruntfile.js
@@ -339,11 +339,11 @@ module.exports = function(grunt) {
           }
         },
         scripts: [
-          '//code.angularjs.org/1.2.6/angular.js',
-          '//code.angularjs.org/1.2.6/angular-touch.js',
+          '//ajax.googleapis.com/ajax/libs/angularjs/1.2.7/angular.js',
+          '//ajax.googleapis.com/ajax/libs/angularjs/1.2.7/angular-touch.js',
         ],
         hiddenScripts: [
-          'http://code.angularjs.org/1.2.6/angular-animate.js',
+          '//ajax.googleapis.com/ajax/libs/angularjs/1.2.7/angular-animate.js',
           'bower_components/google-code-prettify/src/prettify.js',
           'node_modules/marked/lib/marked.js'
         ],
