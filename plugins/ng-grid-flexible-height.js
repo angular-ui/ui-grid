37281bc425b2de18c820df4453d9736b42b71e63
David Sanders <contactdsanders@gmail.com>
2014-01-15 11:22:29 -0500

    Fixing the viewport height math - fixes scrollbar
    The calculation for newViewportHeight is off by 1px in webkit-based browsers. In cases where the actual grid height is less than the grid's max-height (even if a max-height hasn't been set) vertical-overflow scrollbars are appended to the DOM. Increasing the newViewPort by 1px corrects this issue.

--- plugins/ng-grid-flexible-height.js
+++ plugins/ng-grid-flexible-height.js
@@ -18,7 +18,7 @@ function ngGridFlexibleHeightPlugin (opts) {
                 }
             }
 
-            var newViewportHeight = naturalHeight + 2;
+            var newViewportHeight = naturalHeight + 3;
             if (!self.scope.baseViewportHeight || self.scope.baseViewportHeight !== newViewportHeight) {
                 self.grid.$viewport.css('height', newViewportHeight + 'px');
                 self.grid.$root.css('height', (newViewportHeight + extraHeight) + 'px');
