cbc1053c8aeffa279cfa3ac3ac57109b700a086f
Brian Hann <emailc0bra@gmail.com>
2014-01-15 15:09:55 -0600

    WIP: customizer

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,6 +82,18 @@ module.exports = function(grunt) {
       dist: {
         src: ['src/js/**/*.js', '.tmp/template.js'],
         dest: '<%= dist %>/release/<%= pkg.name %>.js'
+      },
+
+      // Concat all the less files together for the customizer
+      customizer_less: {
+        options: {
+          process: function(src, filepath) {
+            // Strip import statements since we're concatting
+            return src.replace(/\@import\s*.+?;/g, '');
+          }
+        },
+        src: 'src/less/**/*.less',
+        dest: '<%= dist %>/less/<%= pkg.name %>.less'
       }
     },
 
