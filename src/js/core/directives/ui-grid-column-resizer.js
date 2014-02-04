ca41ed8a8441c92787f7fdaee1047076e663e2d8
Brian Hann <emailc0bra@gmail.com>
2014-02-04 14:28:51 -0600

    Show resizer overlay; move along with mouse

--- src/js/core/directives/ui-grid-column-resizer.js
+++ src/js/core/directives/ui-grid-column-resizer.js
@@ -2,18 +2,39 @@
   'use strict';
 
   angular.module('ui.grid').directive('uiGridColumnResizer', ['$log', '$document', function ($log, $document) {
+    var resizeOverlay = angular.element('<div class="ui-grid-resize-overlay"></div>');
+
     var resizer = {
       priority: 0,
       scope: {
         col: '='
       },
       require: '?^uiGrid',
-      link: function ($scope, $elm, $attrs) {
-        function mousemove(event, args) {
+      link: function ($scope, $elm, $attrs, uiGridCtrl) {
+        var startX = 0,
+            x = 0,
+            gridLeft = 0;
 
+        function mousemove(event, args) {
+          if (event.originalEvent) { event = event.originalEvent; }
+          event.preventDefault();
+
+          x = event.clientX - gridLeft;
+
+          if (x < 0) { x = 0; }
+          else if (x > uiGridCtrl.grid.gridWidth) { x = uiGridCtrl.grid.gridWidth; }
+
+          resizeOverlay.css({ left: x });
         }
 
         function mouseup(event, args) {
+          if (event.originalEvent) { event = event.originalEvent; }
+          event.preventDefault();
+
+          uiGridCtrl.grid.element.removeClass('column-resizing');
+
+          resizeOverlay.remove();
+
           // Resize the column
 
           $document.off('mouseup', mouseup);
@@ -21,8 +42,24 @@
         }
 
         $elm.on('mousedown', function(event, args) {
+          if (event.originalEvent) { event = event.originalEvent; }
           event.preventDefault();
 
+          uiGridCtrl.grid.element.addClass('column-resizing');
+
+          // Get the left offset of the grid
+          gridLeft = uiGridCtrl.grid.element[0].offsetLeft;
+
+          // Get the starting X position, which is the X coordinate of the click minus the grid's offset
+          startX = event.clientX - gridLeft;
+
+          // Append the resizer overlay
+          uiGridCtrl.grid.element.append(resizeOverlay);
+
+          // Place the resizer overlay at the start position
+          resizeOverlay.css({ left: startX });
+
+          // Add handlers for mouse move and up events
           $document.on('mouseup', mouseup);
           $document.on('mousemove', mousemove);
         });
