74935a9521eef4e27767295b177a6f9688232961
Krooted <kroot13g@gmail.com>
2014-02-12 13:41:56 -0500

    Exporting objects and Custom Column Headers
    I needed the ability to export my custom grid headers to excel(WYSIWYG).  I also needed the ability to export a specific property off of a child object that was embedded in my collection of objects.
    
    Example:
    $scope.myData = [{name: "Moroni", age: 50, features:{eyeColor:"brown", hairColor:"black"}},
                     {name: "Tiancum", age: 43, features:{eyeColor:"blue", hairColor:"black"}},
                     {name: "Jacob", age: 27, features:{eyeColor:"hazel", hairColor:"brunette"}},
                     {name: "Nephi", age: 29, features:{eyeColor:"brown", hairColor:"blonde"}},
                     {name: "Enos", age: 34, features:{eyeColor:"brown", hairColor:"red"}}];
    
    Exporting features.eyeColor 
    
    Please see my before and after Plunkers
    Before: http://plnkr.co/edit/EKSHFYR5iKeax5fmSVN2?p=preview
    After: http://plnkr.co/edit/0pRgu6r8hMOibUkgx12L?p=preview

--- plugins/ng-grid-csv-export.js
+++ plugins/ng-grid-csv-export.js
@@ -3,19 +3,33 @@
 // 2) add a config option for IE users which takes a URL.  That URL should accept a POST request with a
 //    JSON encoded object in the payload and return a CSV.  This is necessary because IE doesn't let you
 //    download from a data-uri link
-//
+//    http://plnkr.co/edit/keci2Y?p=preview
 // Notes:  This has not been adequately tested and is very much a proof of concept at this point
+
+//see Kyle changes on plunker http://plnkr.co/edit/0pRgu6r8hMOibUkgx12L?p=preview
 function ngGridCsvExportPlugin (opts) {
     var self = this;
     self.grid = null;
     self.scope = null;
     self.init = function(scope, grid, services) {
         self.grid = grid;
-        self.scope = scope;
+        self.scope = scope; 
         function showDs() {
             var keys = [];
-            for (var f in grid.config.columnDefs) { keys.push(grid.config.columnDefs[f].field);}
-            var csvData = '';
+            var csvData = '';/**Kyle, moved so that it doesnt get reset before loop**/
+            for (var f in grid.config.columnDefs) {
+            	keys.push(grid.config.columnDefs[f].field);
+            	
+            	csvData += '"' ;/**Kyle Added**/
+            	if(typeof grid.config.columnDefs[f].displayName !== 'undefined'){/** moved to reduce looping and capture the display name if it exists**/
+            		csvData += csvStringify(grid.config.columnDefs[f].displayName);/**Kyle Added**/
+            	}
+            	else{/**Kyle Added**/
+            		csvData += csvStringify(grid.config.columnDefs[f].field);/**Kyle Added**/
+            	}
+            	csvData +=  '",'; /**Kyle Added**/
+            }
+            
             function csvStringify(str) {
                 if (str == null) { // we want to catch anything null-ish, hence just == not ===
                     return '';
@@ -36,29 +50,40 @@ function ngGridCsvExportPlugin (opts) {
                 var newStr = str.substr(0,str.length - 1);
                 return newStr + "\n";
             }
-            for (var k in keys) {
-                csvData += '"' + csvStringify(keys[k]) + '",';
-            }
+            /**unnecessary loop**/
+            /**for (var k in keys) {
+            	csvData += '"' + csvStringify(keys[k]) + '",';
+            }**/
             csvData = swapLastCommaForNewline(csvData);
             var gridData = grid.data;
             for (var gridRow in gridData) {
                 for ( k in keys) {
                     var curCellRaw;
-                    if (opts != null && opts.columnOverrides != null && opts.columnOverrides[keys[k]] != null) {
-                        curCellRaw = opts.columnOverrides[keys[k]](gridData[gridRow][keys[k]]);
-                    }
-                    else {
-                        curCellRaw = gridData[gridRow][keys[k]];
-                    }
-                    csvData += '"' + csvStringify(curCellRaw) + '",';
+                    /**this still needs a case for arrays**/
+                    if(keys[k]){/**Kyle added**/
+                    	if (opts != null && opts.columnOverrides != null && opts.columnOverrides[keys[k]] != null) {
+	                        curCellRaw = opts.columnOverrides[keys[k]](gridData[gridRow][keys[k]]);
+	                    }
+	                    else if (opts != null && opts.columnOverrides != null && keys[k].indexOf(".") >= 0) {/**Kyle modified**/
+	                    	var tempObj = keys[k].substring(0, keys[k].indexOf("."));/**Kyle added**/
+	                    	var tempProp = keys[k].substring(keys[k].indexOf(".")+1, keys[k].length);/**Kyle added**/
+	                    	if(typeof gridData[gridRow][tempObj] !== 'undefined'){/**Kyle**/
+	                    		curCellRaw = opts.columnOverrides['object'](gridData[gridRow][tempObj], tempProp);/**Kyle modified**/
+	                    	}
+	                    }
+	                    else {
+	                        curCellRaw = gridData[gridRow][keys[k]];/**Kyle modified**/
+	                    }
+                    	csvData += '"' + csvStringify(curCellRaw) + '",';
+                    }/**Kyle added**/
                 }
                 csvData = swapLastCommaForNewline(csvData);
             }
             var fp = grid.$root.find(".ngFooterPanel");
             var csvDataLinkPrevious = grid.$root.find('.ngFooterPanel .csv-data-link-span');
             if (csvDataLinkPrevious != null) {csvDataLinkPrevious.remove() ; }
-            var csvDataLinkHtml = "<span class=\"csv-data-link-span\">";
-            csvDataLinkHtml += "<br><a href=\"data:text/csv;charset=UTF-8,";
+            var csvDataLinkHtml = "<span class=\"csv-data-link-span\" style='float:right;'>";
+            csvDataLinkHtml += "<br><a class=\"btn btn-primary\" href=\"data:text/csv;charset=UTF-8,";
             csvDataLinkHtml += encodeURIComponent(csvData);
             csvDataLinkHtml += "\" download=\"Export.csv\">CSV Export</a></br></span>" ;
             fp.append(csvDataLinkHtml);
@@ -71,7 +96,7 @@ function ngGridCsvExportPlugin (opts) {
             }
             return hash;
         };
-        if (opts.customDataWatcher) {
+        if (opts && opts.customDataWatcher) {
             scope.$watch(opts.customDataWatcher, showDs);
         } else {
             scope.$watch(scope.catHashKeys, showDs);
