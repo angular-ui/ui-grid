b003eb8e9178709b4a7134cd4d5dfc14c1928eff
Brian Hann <emailc0bra@gmail.com>
2014-02-24 10:56:59 -0600

    Fixed memory leak. Had to manually remove references to DOM elements in grid instance

--- src/services/DomUtilityService.js
+++ src/services/DomUtilityService.js
@@ -36,11 +36,26 @@
         //Footers
         grid.$footerPanel = grid.$root.find(".ngFooterPanel");
 
-        $scope.$on('$destroy', $scope.$watch(function () {
+        var scopeDereg = $scope.$watch(function () {
             return grid.$viewport.scrollLeft();
         }, function (newLeft) {
             return grid.$headerContainer.scrollLeft(newLeft);
-        }));
+        });
+
+        $scope.$on('$destroy', function() {
+            // Remove all references to DOM elements, otherwise we get memory leaks
+            grid.$root = null;
+            grid.$topPanel = null;
+            grid.$groupPanel = null;
+            grid.$headerContainer = null;
+            grid.$headerScroller = null;
+            grid.$headers = null;
+            grid.$canvas = null;
+            grid.$footerPanel = null;
+
+            scopeDereg();
+        });
+
         domUtilityService.UpdateGridLayout($scope, grid);
     };
     domUtilityService.getRealWidth = function (obj) {
