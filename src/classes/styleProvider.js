986d5b424afa89f02cc91b2af94698bff3c45191
getuliojr <gromao@gmail.com>
2014-03-13 14:37:28 -0300

    Update how grid is rendered
    This is where grid will be rendered, so here there is more patches to fix the height problem.

--- src/classes/styleProvider.js
+++ src/classes/styleProvider.js
@@ -3,6 +3,19 @@
         return { "height": col.headerRowHeight + "px" };
     };
     $scope.rowStyle = function (row) {
+        //Find out highest cell height
+        var rowHeight = $scope.rowHeight;
+        var cols = row.elm.context.children.length;
+        for (var r = 0; r < cols; r++) {
+            if (rowHeight < row.elm.context.children[r].scrollHeight)
+                rowHeight = row.elm.context.children[r].scrollHeight;
+        }
+        //Fix offsetTop of next cell
+        if (grid.rowCache[row.rowIndex + 1] != null)
+            grid.rowCache[row.rowIndex + 1].clone.offsetTop = row.offsetTop + rowHeight;
+        else //Fix grid height
+            grid.$viewport[0].style.height = row.offsetTop + rowHeight + 20 + "px";
+
         var ret = { "top": row.offsetTop + "px", "height": $scope.rowHeight + "px" };
         if (row.isAggRow) {
             ret.left = row.offsetLeft;
@@ -10,7 +23,8 @@
         return ret;
     };
     $scope.canvasStyle = function() {
-        return { "height": grid.maxCanvasHt + "px" };
+        //Not necessary to set height anymore. All function could be erased
+        return "";
     };
     $scope.headerScrollerStyle = function() {
         return { "height": grid.config.headerRowHeight + "px" };
