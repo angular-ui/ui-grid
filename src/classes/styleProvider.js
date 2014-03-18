d3265bcb87bc984d6b5d24d8c4023c7fef3a28a0
getuliojr <gromao@gmail.com>
2014-03-18 12:37:56 -0300

    Recreate routine rowStyle for correct row Size
    There was an error in the first version and also no optimization and it was making the $watch go crazy on large data changes. All data has been fixed in this version.
    
    Now you can resize and all content still fits screen.

--- src/classes/styleProvider.js
+++ src/classes/styleProvider.js
@@ -6,9 +6,18 @@
         //Find out highest cell height
         var rowHeight = $scope.rowHeight;
         var cols = row.elm.context.children.length;
+        var getRowContentHeight = function (row) {
+            var height = 0;
+            angular.element.swap(row, { height: 0, overflow: "scroll" }, function () {
+                height = row.scrollHeight;
+            });
+            return height;
+        }
+        //Get highest row size
         for (var r = 0; r < cols; r++) {
-            if (rowHeight < row.elm.context.children[r].scrollHeight)
-                rowHeight = row.elm.context.children[r].scrollHeight;
+            var newRowContentHeight = getRowContentHeight(row.elm.context.children[r]);
+            if (rowHeight < newRowContentHeight)
+                rowHeight = newRowContentHeight;
         }
         //Fix offsetTop of next cell
         if (grid.rowCache[row.rowIndex + 1] != null)
@@ -16,7 +25,7 @@
         else //Fix grid height
             grid.$viewport[0].style.height = row.offsetTop + rowHeight + 20 + "px";
 
-        var ret = { "top": row.offsetTop + "px", "height": $scope.rowHeight + "px" };
+        var ret = { "top": row.offsetTop + "px", "height": rowHeight + "px" };
         if (row.isAggRow) {
             ret.left = row.offsetLeft;
         }
