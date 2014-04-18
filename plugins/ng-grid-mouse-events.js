6a6bf70012c59c159e7081abc7f75b473a8d90cd
Eugen Istoc <eugenistoc@gmail.com>
2014-04-18 11:12:59 -0400

    Mouse Events Plugin

---
+++ plugins/ng-grid-mouse-events.js
@@ -0,0 +1,33 @@
+function ngGridDoubleClickPlugin() {
+    var self = this;
+    self.$scope = null;
+    self.grid = null;
+
+    self.init = function (scope, grid, services) {
+        self.$scope = scope;
+        self.grid = grid;
+
+        self.assignEvents();
+    };
+
+    self.assignEvents = function () {
+        self.grid.$viewport.on('click', self.onClick);
+        self.grid.$viewport.on('dblclick', self.onDblClick);
+        self.grid.$viewport.on('contextmenu', self.onRightClick);
+    };
+
+    self.onClick = function () {
+        if (typeof self.grid.config.onClick == 'function')
+            self.grid.config.onClick(self.$scope.selectedItems[0]);
+    };
+
+    self.onDblClick = function (row) {
+        if (typeof self.grid.config.onDblClick == 'function')
+            self.grid.config.onDblClick(self.$scope.selectedItems[0]);
+    };
+
+    self.onRightClick = function () {
+        if (typeof self.grid.config.onRightClick == 'function')
+            self.grid.config.onRightClick(self.$scope.selectedItems[0]);
+    };
+}
