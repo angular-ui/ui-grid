0cede595d07a29e66364b00ca9f0bfbf3e350d45
Shane Walters <swalters@singletreetech.com>
2014-07-23 16:08:16 -0500

    WIP: more public api changes.

---
+++ src/js/core/factories/GridApi.js
@@ -0,0 +1,167 @@
+(function () {
+
+  angular.module('ui.grid')
+    .factory('GridApi', ['$log', '$q', '$rootScope', 'gridUtil', 'uiGridConstants',
+      function ($log, $q, $rootScope, gridUtil, uiGridConstants) {
+
+
+        /**
+         * @ngdoc function
+         * @name ui.grid.class:GridApi
+         * @description GridApi provides the ability to register public methods events inside the grid and allow
+         * for other components to use the api via featureName.methodName and featureName.on.eventName(function(args){}
+         * @param {object} grid grid that owns api
+         */
+        var GridApi = function GridApi(grid) {
+          this.grid = grid;
+          this.listeners = [];
+        };
+
+        GridApi.prototype.suppressEvents = function (listenerFuncs, callBackFn) {
+          var self = this;
+          var listeners = angular.isArray(listenerFuncs) ? listenerFuncs : [listenerFuncs];
+
+          //find all registered listeners
+          var foundListeners = [];
+          listeners.forEach(function (l) {
+            foundListeners = self.listeners.filter(function (lstnr) {
+              return l === lstnr.handler;
+            });
+          });
+
+          //deregister all the listeners
+          foundListeners.forEach(function(l){
+            l.dereg();
+          });
+
+          callBackFn();
+
+          //reregister all the listeners
+          foundListeners.forEach(function(l){
+              l.dereg = registerEventWithAngular(l.scope, l.eventId, l.handler);
+          });
+
+        };
+
+        /**
+         * @ngdoc function
+         * @name registerEvent
+         * @methodOf ui.grid.class:GridApi
+         * @description Registers a new event for the given feature
+         * @param {string} featureName name of the feature that raises the event
+         * @param {string} eventName  name of the event
+         */
+        GridApi.prototype.registerEvent = function (featureName, eventName) {
+          var self = this;
+          if (!self[featureName]) {
+            self[featureName] = {};
+          }
+
+          var feature = self[featureName];
+          if (!feature.on) {
+            feature.on = {};
+            feature.raise = {};
+          }
+
+          var eventId = self.grid.id + featureName + eventName;
+
+          $log.log('Creating raise event method ' + featureName + '.raise.' + eventName);
+          feature.raise[eventName] = function () {
+            $rootScope.$broadcast.apply($rootScope, [eventId].concat(Array.prototype.slice.call(arguments)));
+          };
+
+          $log.log('Creating on event method ' + featureName + '.on.' + eventName);
+          feature.on[eventName] = function (scope, handler) {
+            var dereg = registerEventWithAngular(scope, eventId, handler);
+
+            //track our listener so we can turn off and on
+            var listener = {handler: handler, dereg: dereg, eventId: eventId, scope: scope};
+            self.listeners.push(listener);
+
+            //destroy tracking when scope is destroyed
+            //wanted to remove the listener from the array but angular does
+            //strange things in scope.$destroy so I could not access the listener array
+            scope.$on('$destroy', function() {
+              listener.dereg = null;
+              listener.handler = null;
+              listener.eventId = null;
+              listener.scope = null;
+            });
+          };
+        };
+
+        function registerEventWithAngular(scope, eventId, handler) {
+          return scope.$on(eventId, function (event) {
+            var args = Array.prototype.slice.call(arguments);
+            args.splice(0, 1); //remove evt argument
+            handler.apply(this.grid, args);
+          });
+        }
+
+        /**
+         * @ngdoc function
+         * @name registerEventsFromObject
+         * @methodOf ui.grid.class:GridApi
+         * @description Registers features and events from a simple objectMap.
+         * eventObjectMap must be in this format (multiple features allowed)
+         * <br>
+         * {featureName:
+         *        {
+         *          eventNameOne:function(args){},
+         *          eventNameTwo:function(args){}
+         *        }
+         * @param {object} eventObjectMap map of feature/event names
+         */
+        GridApi.prototype.registerEventsFromObject = function (eventObjectMap) {
+          var self = this;
+          var features = [];
+          angular.forEach(eventObjectMap, function (featProp, featPropName) {
+            var feature = {name: featPropName, events: []};
+            angular.forEach(featProp, function (prop, propName) {
+              feature.events.push(propName);
+            });
+            features.push(feature);
+          });
+
+          features.forEach(function (feature) {
+            feature.events.forEach(function (event) {
+              self.registerEvent(feature.name, event);
+            });
+          });
+
+        };
+
+        GridApi.prototype.registerMethod = function (featureName, methodName, callBackFn) {
+          if (!this[featureName]) {
+            this[featureName] = {};
+          }
+
+          var feature = this[featureName];
+          feature[methodName] = callBackFn;
+
+        };
+
+        GridApi.prototype.registerMethodsFromObject = function (methodMap) {
+          var self = this;
+          var features = [];
+          angular.forEach(methodMap, function (featProp, featPropName) {
+            var feature = {name: featPropName, methods: []};
+            angular.forEach(featProp, function (prop, propName) {
+              feature.methods.push({name: propName, fn: prop});
+            });
+            features.push(feature);
+          });
+
+          features.forEach(function (feature) {
+            feature.methods.forEach(function (method) {
+              self.registerMethod(feature.name, method.name, method.fn);
+            });
+          });
+
+        };
+
+        return GridApi;
+
+      }]);
+
+})();
