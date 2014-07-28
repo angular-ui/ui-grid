6f13e65b09efdff5220c890f15971b0cb2af31bc
Zacharias3690 <Zacharias3690@gmail.com>
2014-07-28 11:27:39 -0500

    add on finish render directive
    Add on on finish render directive that fires an event when rows finish
    rendering. Allows for easily implementing loading widgets and other
    manipulation after rendering.

---
+++ src/directives/ng-on-finish-render.js
@@ -0,0 +1,14 @@
+ngGridDirectives.directive('ngOnFinishRender', [
+    '$timeout', function($timeout) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attr) {
+                if (scope.$last === true) {
+                    $timeout(function() {
+                        scope.$emit('ngGridFinishRender', element);
+                    });
+                }
+            }
+        }
+    }
+]);
