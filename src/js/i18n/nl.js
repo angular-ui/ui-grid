0861bcd6d911266c6c75f8c7c5db0004dd7e5d09
job <job@manduro.nl>
2014-08-04 10:19:26 +0200

    Add Dutch translation

---
+++ src/js/i18n/nl.js
@@ -0,0 +1,34 @@
+(function () {
+  angular.module('ui.grid').config(['$provide', function($provide) {
+    $provide.decorator('i18nService', ['$delegate', function($delegate) {
+      $delegate.add('nl', {
+        aggregate: {
+          label: 'items'
+        },
+        groupPanel: {
+          description: 'Sleep hier een kolomnaam heen om op te groeperen.'
+        },
+        search: {
+          placeholder: 'Zoeken...',
+          showingItems: 'Getoonde items:',
+          selectedItems: 'Geselecteerde items:',
+          totalItems: 'Totaal aantal items:',
+          size: 'Items per pagina:',
+          first: 'Eerste pagina',
+          next: 'Volgende pagina',
+          previous: 'Vorige pagina',
+          last: 'Laatste pagina'
+        },
+        menu: {
+          text: 'Kies kolommen:'
+        },
+        sort: {
+          ascending: 'Sorteer oplopend',
+          descending: 'Sorteer aflopend',
+          remove: 'Verwijder sortering'
+        }
+      });
+      return $delegate;
+    }]);
+  }]);
+})();
