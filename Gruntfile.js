e32fd8abb8ed2bd0e267c6b07e0e54ae44edea86
c0bra <c0bra@users.noreply.github.com>
2014-09-24 17:07:33 -0500

    Adding jit-grunt mapping for jscs task

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,7 @@ module.exports = function(grunt) {
     ngtemplates: 'grunt-angular-templates',
     changelog: 'grunt-conventional-changelog',
     shell: 'grunt-shell-spawn',
+    jscs: 'grunt-jscs-checker',
     'stable-version': './lib/grunt/plugins.js',
     'current-version': './lib/grunt/plugins.js'
   });
