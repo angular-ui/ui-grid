15775542f3e8f29ef6b9a8f044d7fdced64e2ae6
Dylan Robinson <dylan@jugnoo.com>
2014-10-07 20:58:50 -0400

    Refactor columnWidth calculations

--- src/js/core/directives/ui-grid-header.js
+++ src/js/core/directives/ui-grid-header.js
@@ -76,12 +76,6 @@
             gridUtil.disableAnimations($elm);
 
             function updateColumnWidths() {
-              var asterisksArray = [],
-                  percentArray = [],
-                  manualArray = [],
-                  asteriskNum = 0,
-                  totalWidth = 0;
-
               // Get the width of the viewport
               var availableWidth = containerCtrl.colContainer.getViewportWidth();
 
@@ -93,186 +87,89 @@
               // var equalWidthColumnCount = columnCount = uiGridCtrl.grid.options.columnDefs.length;
               // var equalWidth = availableWidth / equalWidthColumnCount;
 
-              // The last column we processed
-              var lastColumn;
-
-              var manualWidthSum = 0;
-
-              var canvasWidth = 0;
-
-              var ret = '';
-
-
-              // uiGridCtrl.grid.columns.forEach(function(column, i) {
-
-              var columnCache = containerCtrl.colContainer.visibleColumnCache;
-
-              columnCache.forEach(function(column, i) {
-                // ret = ret + ' .grid' + uiGridCtrl.grid.id + ' .col' + i + ' { width: ' + equalWidth + 'px; left: ' + left + 'px; }';
-                //var colWidth = (typeof(c.width) !== 'undefined' && c.width !== undefined) ? c.width : equalWidth;
-
-                // Skip hidden columns
-                if (!column.visible) { return; }
-
-                var colWidth,
-                    isPercent = false;
-
-                if (!angular.isNumber(column.width)) {
-                  isPercent = isNaN(column.width) ? gridUtil.endsWith(column.width, "%") : false;
+              var columnCache = containerCtrl.colContainer.visibleColumnCache,
+                  canvasWidth = 0,
+                  asteriskNum = 0,
+                  hasVariableWidth = false;
+              
+              var getColWidth = function(column){
+                if (column.widthType === "manual"){ 
+                  return +column.width; 
                 }
-
-                if (angular.isString(column.width) && column.width.indexOf('*') !== -1) { //  we need to save it until the end to do the calulations on the remaining width.
-                  asteriskNum = parseInt(asteriskNum + column.width.length, 10);
-                  
-                  asterisksArray.push(column);
+                else if (column.widthType === "percent"){ 
+                  return parseInt(column.width.replace(/%/g, ''), 10) * availableWidth / 100;
                 }
-                else if (isPercent) { // If the width is a percentage, save it until the very last.
-                  percentArray.push(column);
+                else if (column.widthType === "auto"){ 
+                  var oneAsterisk = parseInt(availableWidth / asteriskNum, 10);
+                  return column.width.length * oneAsterisk; 
                 }
-                else if (angular.isNumber(column.width)) {
-                  manualWidthSum = parseInt(manualWidthSum + column.width, 10);
-                  
-                  canvasWidth = parseInt(canvasWidth, 10) + parseInt(column.width, 10);
-
-                  column.drawnWidth = column.width;
-
+              };
+              
+              // Populate / determine column width types:
+              columnCache.forEach(function(column){
+                column.widthType = null;
+                if (isFinite(+column.width)){
+                  column.widthType = "manual";
+                }
+                else if (gridUtil.endsWith(column.width, "%")){
+                  column.widthType = "percent";
+                  hasVariableWidth = true;
+                }
+                else if (angular.isString(column.width) && column.width.indexOf('*') !== -1){
+                  column.widthType = "auto";
+                  asteriskNum += column.width.length;
+                  hasVariableWidth = true;
                 }
               });
-
-              // Get the remaining width (available width subtracted by the manual widths sum)
-              var remainingWidth = availableWidth - manualWidthSum;
-
-              var i, column, colWidth;
-
-              if (percentArray.length > 0) {
-                // Pre-process to make sure they're all within any min/max values
-                for (i = 0; i < percentArray.length; i++) {
-                  column = percentArray[i];
-
-                  var percent = parseInt(column.width.replace(/%/g, ''), 10) / 100;
-
-                  colWidth = parseInt(percent * remainingWidth, 10);
-
-                  if (column.colDef.minWidth && colWidth < column.colDef.minWidth) {
-                    colWidth = column.colDef.minWidth;
-
-                    remainingWidth = remainingWidth - colWidth;
-
-                    canvasWidth += colWidth;
-                    column.drawnWidth = colWidth;
-
-                    // Remove this element from the percent array so it's not processed below
-                    percentArray.splice(i, 1);
-                  }
-                  else if (column.colDef.maxWidth && colWidth > column.colDef.maxWidth) {
-                    colWidth = column.colDef.maxWidth;
-
-                    remainingWidth = remainingWidth - colWidth;
-
-                    canvasWidth += colWidth;
-                    column.drawnWidth = colWidth;
-
-                    // Remove this element from the percent array so it's not processed below
-                    percentArray.splice(i, 1);
-                  }
+              
+              // For sorting, calculate width from first to last:
+              var colWidthPriority = ["manual", "percent", "auto"];
+              columnCache.filter(function(column){
+                // Only draw visible items with a widthType
+                return (column.visible && column.widthType); 
+              }).sort(function(a,b){
+                // Calculate widths in order, so that manual comes first, etc.
+                return colWidthPriority.indexOf(a.widthType) - colWidthPriority.indexOf(b.widthType);
+              }).forEach(function(column){
+                // Calculate widths:
+                var colWidth = getColWidth(column);
+                if (column.minWidth){
+                  colWidth = Math.max(colWidth, column.minWidth);
                 }
-
-                percentArray.forEach(function(column) {
-                  var percent = parseInt(column.width.replace(/%/g, ''), 10) / 100;
-                  var colWidth = parseInt(percent * remainingWidth, 10);
-
-                  canvasWidth += colWidth;
-
-                  column.drawnWidth = colWidth;
-                });
-              }
-
-              if (asterisksArray.length > 0) {
-                var asteriskVal = parseInt(remainingWidth / asteriskNum, 10);
-
-                 // Pre-process to make sure they're all within any min/max values
-                for (i = 0; i < asterisksArray.length; i++) {
-                  column = asterisksArray[i];
-
-                  colWidth = parseInt(asteriskVal * column.width.length, 10);
-
-                  if (column.colDef.minWidth && colWidth < column.colDef.minWidth) {
-                    colWidth = column.colDef.minWidth;
-
-                    remainingWidth = remainingWidth - colWidth;
-                    asteriskNum--;
-
-                    canvasWidth += colWidth;
-                    column.drawnWidth = colWidth;
-
-                    lastColumn = column;
-
-                    // Remove this element from the percent array so it's not processed below
-                    asterisksArray.splice(i, 1);
-                  }
-                  else if (column.colDef.maxWidth && colWidth > column.colDef.maxWidth) {
-                    colWidth = column.colDef.maxWidth;
-
-                    remainingWidth = remainingWidth - colWidth;
-                    asteriskNum--;
-
-                    canvasWidth += colWidth;
-                    column.drawnWidth = colWidth;
-
-                    // Remove this element from the percent array so it's not processed below
-                    asterisksArray.splice(i, 1);
-                  }
+                if (column.maxWidth){
+                  colWidth = Math.min(colWidth, column.maxWidth);
                 }
-
-                // Redo the asterisk value, as we may have removed columns due to width constraints
-                asteriskVal = parseInt(remainingWidth / asteriskNum, 10);
-
-                asterisksArray.forEach(function(column) {
-                  var colWidth = parseInt(asteriskVal * column.width.length, 10);
-
-                  canvasWidth += colWidth;
-
-                  column.drawnWidth = colWidth;
-                });
-              }
-
+                column.drawnWidth = Math.floor(colWidth);
+                canvasWidth += column.drawnWidth;
+              });
+              
               // If the grid width didn't divide evenly into the column widths and we have pixels left over, dole them out to the columns one by one to make everything fit
-              var leftoverWidth = availableWidth - parseInt(canvasWidth, 10);
+              var leftoverWidth = availableWidth - canvasWidth;
 
-              if (leftoverWidth > 0 && canvasWidth > 0 && canvasWidth < availableWidth) {
-                var variableColumn = false;
-                // uiGridCtrl.grid.columns.forEach(function(col) {
-                columnCache.forEach(function(col) {
-                  if (col.width && !angular.isNumber(col.width)) {
-                    variableColumn = true;
+              if (hasVariableWidth && leftoverWidth > 0 && canvasWidth > 0 && canvasWidth < availableWidth) {
+                var prevLeftover = leftoverWidth;
+                var remFn = function (column) {
+                  if (leftoverWidth > 0 && column.widthType === "auto" || column.widthType === "percent") {
+                    column.drawnWidth = column.drawnWidth + 1;
+                    canvasWidth = canvasWidth + 1;
+                    leftoverWidth--;
                   }
-                });
-
-                if (variableColumn) {
-                  var remFn = function (column) {
-                    if (leftoverWidth > 0) {
-                      column.drawnWidth = column.drawnWidth + 1;
-                      canvasWidth = canvasWidth + 1;
-                      leftoverWidth--;
-                    }
-                  };
-                  while (leftoverWidth > 0) {
-                    columnCache.forEach(remFn);
-                  }
+                };
+                while (leftoverWidth > 0 && leftoverWidth !== prevLeftover ) {
+                  prevLeftover = leftoverWidth;
+                  columnCache.forEach(remFn);
                 }
               }
-
-              if (canvasWidth < availableWidth) {
-                canvasWidth = availableWidth;
-              }
+              canvasWidth = Math.max(canvasWidth, availableWidth);
 
               // Build the CSS
               // uiGridCtrl.grid.columns.forEach(function (column) {
+              var ret = '';
               columnCache.forEach(function (column) {
                 ret = ret + column.getColClassDefinition();
               });
 
-              // Add the vertical scrollbar width back in to the canvas width, it's taken out in getCanvasWidth
+              // Add the vertical scrollbar width back in to the canvas width, it's taken out in getViewportWidth
               if (grid.verticalScrollbarWidth) {
                 canvasWidth = canvasWidth + grid.verticalScrollbarWidth;
               }
