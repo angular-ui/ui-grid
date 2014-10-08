3bc3f272986c2cae55f26bc6ef8dd8f5fa6ef401
c0bra <c0bra@users.noreply.github.com>
2014-10-08 10:14:47 -0500

    fix(Travis): Make travis run saucelabs tests
    After 2bdd734b5e28 it looks like the serialsauce task isn't getting run on
    travis, so we haven't been doing any cross-browser testing for a month...
    
    Hopefully this change will do the right if-checks in bash and let the
    trask run.

--- lib/sauce/sauce_connect_block.sh
+++ lib/sauce/sauce_connect_block.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! $TRAVIS_PULL_REQUEST ]
+if [ ! $TRAVIS_PULL_REQUEST == "true" ]
 then
   # Wait for Connect to be ready before exiting
   while [ ! -f $SAUCE_CONNECT_READY_FILE ]; do
