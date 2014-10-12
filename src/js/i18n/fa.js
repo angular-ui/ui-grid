52e9f57653e84fe5bd7de88090c2b4ec3f0da8bf
Paul Lambert <paul@planar.id.au>
2014-10-13 06:57:28 +1300

    Feat(importer): create importer, bug fixes to exporter, extension to rowEdit
    Importer provides the ability to import csv and json files from the file system
    into your application.  If the rowEdit feature is enabled it will also save that
    data to your server.
    
    Minor edits to gridMenu to fix defaulting issues and treatment of show/hide columns
    when columnDefs are present but data is not.
    
    Extended rowEdit to have a `setRowsDirty` method that permits rows to be marked
    as dirty programatically.

--- src/js/i18n/fa.js
+++ src/js/i18n/fa.js
@@ -34,12 +34,20 @@
         },
         gridMenu: {
           columns: 'Columns:',
+          importerTitle: 'Import file',
           exporterAllAsCsv: 'Export all data as csv',
           exporterVisibleAsCsv: 'Export visible data as csv',
           exporterSelectedAsCsv: 'Export selected data as csv',
           exporterAllAsPdf: 'Export all data as pdf',
           exporterVisibleAsPdf: 'Export visible data as pdf',
           exporterSelectedAsPdf: 'Export selected data as pdf'
+        },
+        importer: {
+          noHeaders: 'Column names were unable to be derived, does the file have a header?',
+          noObjects: 'Objects were not able to be derived, was there data in the file other than headers?',
+          invalidCsv: 'File was unable to be processed, is it valid CSV?',
+          invalidJson: 'File was unable to be processed, is it valid Json?',
+          jsonNotArray: 'Imported json file must contain an array, aborting.'
         }
       });
       return $delegate;
