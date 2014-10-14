1408db05fbad99b2d5e4c0d185ef94d4d4b340f4
job <job@manduro.nl>
2014-10-14 09:55:39 +0200

    Dutch translation updates and additions

--- src/js/i18n/nl.js
+++ src/js/i18n/nl.js
@@ -31,28 +31,33 @@
           hide: 'Verberg kolom'
         },
         aggregation: {
-          count: 'aantal rijen: ',
-          sum: 'som: ',
-          avg: 'gemiddelde: ',
-          min: 'min: ',
-          max: 'max: '
+          count: 'Aantal rijen: ',
+          sum: 'Som: ',
+          avg: 'Gemiddelde: ',
+          min: 'Min: ',
+          max: 'Max: '
+        },
+        pinning: {
+          pinLeft: 'Zet links vast',
+          pinRight: 'Zet rechts vast',
+          unpin: 'Maak los'
         },
         gridMenu: {
-          columns: 'Columns:',
-          importerTitle: 'Import file',
-          exporterAllAsCsv: 'Export all data as csv',
-          exporterVisibleAsCsv: 'Export visible data as csv',
-          exporterSelectedAsCsv: 'Export selected data as csv',
-          exporterAllAsPdf: 'Export all data as pdf',
-          exporterVisibleAsPdf: 'Export visible data as pdf',
-          exporterSelectedAsPdf: 'Export selected data as pdf'
+          columns: 'Kolommen:',
+          importerTitle: 'Importeer bestand',
+          exporterAllAsCsv: 'Exporteer alle data als csv',
+          exporterVisibleAsCsv: 'Exporteer zichtbare data als csv',
+          exporterSelectedAsCsv: 'Exporteer geselecteerde data als csv',
+          exporterAllAsPdf: 'Exporteer alle data als pdf',
+          exporterVisibleAsPdf: 'Exporteer zichtbare data als pdf',
+          exporterSelectedAsPdf: 'Exporteer geselecteerde data als pdf'
         },
         importer: {
-          noHeaders: 'Column names were unable to be derived, does the file have a header?',
-          noObjects: 'Objects were not able to be derived, was there data in the file other than headers?',
-          invalidCsv: 'File was unable to be processed, is it valid CSV?',
-          invalidJson: 'File was unable to be processed, is it valid Json?',
-          jsonNotArray: 'Imported json file must contain an array, aborting.'
+          noHeaders: 'Kolomnamen kunnen niet worden afgeleid. Heeft het bestand een header?',
+          noObjects: 'Objecten kunnen niet worden afgeleid. Bevat het bestand data naast de headers?',
+          invalidCsv: 'Het bestand kan niet verwerkt worden. Is het een valide csv bestand?',
+          invalidJson: 'Het bestand kan niet verwerkt worden. Is het valide json?',
+          jsonNotArray: 'Het json bestand moet een array bevatten. De actie wordt geannuleerd.'
         }
       });
       return $delegate;
