2c21ccb1283defb3515f667a1367f3048e17699c
c0bra <c0bra@users.noreply.github.com>
2014-10-16 12:16:06 -0500

    Remove extra call to grunt test for PRs [skip ci]

--- travis_build.sh
+++ travis_build.sh
@@ -2,27 +2,13 @@
 
 set -e
 
-# NOTE: Travis build matrix won't let us run something after ALL matrix jobs succeed, so we have to run serially (below)
-# if [ $JOB = "unit" ]; then
-#   grunt
-#   grunt test:ci
-#   grunt release
-# elif [ $JOB = "e2e" ]; then
-#   # grunt clean build test:e2e --browsers=SL_Chrome
-#   grunt
-#   grunt test:e2e:ci
-# else
-#   echo "Unknown job type. Please set JOB=unit or JOB=e2e."
-# fi
-
 grunt
-# grunt test:e2e:ci --verbose
 
 # If this is a pull request then we won't have access to secure variables and can't do integration tests with SauceLabs.
 #   In this case just do normal local tests
 if [ $TRAVIS_PULL_REQUEST != "false" ]
 then
-  grunt test:single
+  # Do nothing on a pull request. The default task already runs the test:single test
 else
   grunt test:ci
 
