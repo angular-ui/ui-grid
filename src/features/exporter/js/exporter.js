83edafbee17e37409d926e292053a32a8ad03914
Thomas Tong <mankintong@gmail.com>
2014-10-24 21:33:21 +0800

    Proper export of date obj to excel readable format

--- src/features/exporter/js/exporter.js
+++ src/features/exporter/js/exporter.js
@@ -629,7 +629,21 @@
           if (typeof(field) === 'string') {
             return '"' + field.replace(/"/g,'""') + '"';
           }
-
+          if (field instanceof Date) {
+            var curr_year = field.getFullYear();
+            var curr_month = field.getMonth() + 1;
+            var curr_date = field.getDate();
+            var curr_hour = field.getHours();
+            var curr_mins = field.getMinutes();
+            var curr_secs = field.getSeconds();
+            if (curr_month < 10) curr_month = "0" + curr_month;
+            if (curr_date < 10) curr_date = "0" + curr_date;
+            if (curr_hour < 10) curr_hour = "0" + curr_hour;
+            if (curr_mins < 10) curr_mins = "0" + curr_mins;
+            if (curr_secs < 10) curr_secs = "0" + curr_secs;
+            return curr_year + "-" + curr_month + "-" + curr_date + " " + curr_hour + ":" + curr_mins + ":" + curr_secs;
+          }
+          
           return JSON.stringify(field);        
         },
 
