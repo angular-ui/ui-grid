angular-ui/ui-grid

commit f552402f1f0576df9f0436868bf0b4987468f0e9

Fixed asterisk widths on columns

src/js/core/directives/ui-grid-header.js
@@ -90,6 +90,7 @@
               var columnCache = containerCtrl.colContainer.visibleColumnCache,
                   canvasWidth = 0,
                   asteriskNum = 0,
+                  leftoverWidth = availableWidth,
                   hasVariableWidth = false;
               
               var getColWidth = function(column){
@@ -100,7 +101,7 @@
                   return parseInt(column.width.replace(/%/g, ''), 10) * availableWidth / 100;
                 }
                 else if (column.widthType === "auto"){ 
-                  var oneAsterisk = parseInt(availableWidth / asteriskNum, 10);
+                  var oneAsterisk = parseInt(leftoverWidth / asteriskNum, 10);
                   return column.width.length * oneAsterisk; 
                 }
               };
@@ -141,11 +142,10 @@
                 }
                 column.drawnWidth = Math.floor(colWidth);
                 canvasWidth += column.drawnWidth;
+                leftoverWidth -= column.drawnWidth;
               });
               
               // If the grid width didn't divide evenly into the column widths and we have pixels left over, dole them out to the columns one by one to make everything fit
-              var leftoverWidth = availableWidth - canvasWidth;
-
               if (hasVariableWidth && leftoverWidth > 0 && canvasWidth > 0 && canvasWidth < availableWidth) {
                 var prevLeftover = leftoverWidth;
                 var remFn = function (column) {

src/js/core/factories/GridColumn.js
@@ -358,7 +358,11 @@ angular.module('ui.grid')
             self.width = parseInt(colDef.width.match(/^(\d+)$/)[1], 10);
           }
           // Otherwise it should be a string of asterisks
-          else if (!colDef.width.match(/^\*+$/)) {
+          else if (colDef.width.match(/^\*+$/)) {
+            self.width = colDef.width;
+          }
+          // No idea, throw an Error
+          else {
             throw new Error(parseErrorMsg);
           }
         }

commit 1b6b3e83c8a0286be0a5ba41ac3a87fc08c7d0e2

fix broken auto-width columns
This fixes a regression introduced by
f552402f1f0576df9f0436868bf0b4987468f0e9

src/js/core/directives/ui-grid-header.js
@@ -90,6 +90,7 @@
               var columnCache = containerCtrl.colContainer.visibleColumnCache,
                   canvasWidth = 0,
                   asteriskNum = 0,
+                  oneAsterisk = 0,
                   leftoverWidth = availableWidth,
                   hasVariableWidth = false;
               
@@ -100,8 +101,12 @@
                 else if (column.widthType === "percent"){ 
                   return parseInt(column.width.replace(/%/g, ''), 10) * availableWidth / 100;
                 }
-                else if (column.widthType === "auto"){ 
-                  var oneAsterisk = parseInt(leftoverWidth / asteriskNum, 10);
+                else if (column.widthType === "auto"){
+                  // leftOverWidth is subtracted from after each call to this
+                  // function so we need to calculate oneAsterisk size only once
+                  if (oneAsterisk === 0) {
+                    oneAsterisk = parseInt(leftoverWidth / asteriskNum, 10);
+                  }
                   return column.width.length * oneAsterisk; 
                 }
               };