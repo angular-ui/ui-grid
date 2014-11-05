369ce68aba6bc315097e5864cfe0fe13e76e7a7a
unknown <bchance@BA-27.neucg.local>
2014-11-05 10:44:26 -0600

    Add ngdocs

--- src/features/paging/js/ui-grid-paging.js
+++ src/features/paging/js/ui-grid-paging.js
@@ -1,18 +1,54 @@
 (function() {
   'use strict';
-
+  /**
+   * @ngdoc overview
+   * @name ui.grid.paging
+   *
+   * @description
+   *
+   * #ui.grid.paging
+   * This module provides paging support to ui-grid
+   */
+   
   var module = angular.module('ui.grid.paging', ['ui.grid']);
 
+  /**
+   * @ngdoc service
+   * @name ui.grid.paging.service:uiGridPagingService
+   *
+   * @description Service for the paging feature
+   */
   module.service('uiGridPagingService', ['gridUtil', 
     function (gridUtil) {
       var service = {
+      /**
+       * @ngdoc method
+       * @name initializeGrid
+       * @methodOf ui.grid.paging.service:uiGridPagingService
+       * @description Attaches the service to a certain grid
+       * @param {Grid} grid The grid we want to work with
+       */
         initializeGrid: function (grid) {
           service.defaultGridOptions(grid.options);
 
+          /**
+          * @ngdoc object
+          * @name ui.grid.paging.api:PublicAPI
+          *
+          * @description Public API for the paging feature
+          */
           var publicApi = {
             events: {
               paging: {
-                pagingChanged: function ($scope, fn) { }
+              /**
+               * @ngdoc event
+               * @name pagingChanged
+               * @eventOf ui.grid.paging.api:PublicAPI
+               * @description This event fires when the pageSize or currentPage changes
+               * @param {newPage} requested page number
+               * @param {pageSize} requested page size 
+               */
+                pagingChanged: function (newPage, pageSize) { }
               }
             },
             methods: {
@@ -37,17 +73,47 @@
 
         },
         defaultGridOptions: function (gridOptions) {
+          /**
+           * @ngdoc property
+           * @name enablePaging
+           * @propertyOf ui.grid.class:GridOptions
+           * @description Enables paging, defaults to true
+           */
           gridOptions.enablePaging = gridOptions.enablePaging !== false;
+          /**
+           * @ngdoc property
+           * @name useExternalPaging
+           * @propertyOf ui.grid.class:GridOptions
+           * @description Disables client side paging. When true, handle the pagingChanged event and set data and totalItems
+           * defaults to false
+           */           
           gridOptions.useExternalPaging = gridOptions.useExternalPaging === true;
-          
+          /**
+           * @ngdoc property
+           * @name totalItems
+           * @propertyOf ui.grid.class:GridOptions
+           * @description Total number of items, set automatically when client side paging, needs set by user for server side paging
+           */
           if (gridUtil.isNullOrUndefined(gridOptions.totalItems)) {
             gridOptions.totalItems = 0;
           }
-
+          /**
+           * @ngdoc property
+           * @name pagingPageSizes
+           * @propertyOf ui.grid.class:GridOptions
+           * @description Array of page sizes
+           * defaults to [250, 500, 1000]
+           */
           if (gridUtil.isNullOrUndefined(gridOptions.pagingPageSizes)) {
             gridOptions.pagingPageSizes = [250, 500, 1000];
           }
-          
+          /**
+           * @ngdoc property
+           * @name pagingPageSize
+           * @propertyOf ui.grid.class:GridOptions
+           * @description Page size
+           * defaults to the first item in pagingPageSizes, or 0 if pagingPageSizes is empty
+           */
           if (gridUtil.isNullOrUndefined(gridOptions.pagingPageSize)) {
             if (gridOptions.pagingPageSizes.length > 0) {
               gridOptions.pagingPageSize = gridOptions.pagingPageSizes[0];
@@ -55,12 +121,26 @@
               gridOptions.pagingPageSize = 0;
             }
           }
-
+          /**
+           * @ngdoc property
+           * @name pagingCurrentPage
+           * @propertyOf ui.grid.class:GridOptions
+           * @description Current page number
+           * default 1
+           */
           if (gridUtil.isNullOrUndefined(gridOptions.pagingCurrentPage)) {
             gridOptions.pagingCurrentPage = 1;
           }
-
         },
+        /**
+         * @ngdoc method
+         * @methodOf ui.grid.paging.service:uiGridPagingService
+         * @name uiGridPagingService
+         * @description  Raises pagingChanged and calls refresh for client side paging
+         * @param {grid} the grid for which the paging changed
+         * @param {newPage} requested page number
+         * @param {pageSize} requested page size 
+         */
         onPagingChanged: function (grid, newPage, pageSize) {
             grid.api.paging.raise.pagingChanged(newPage, pageSize);
             if (!grid.options.useExternalPaging) {
@@ -72,7 +152,52 @@
       return service;
     }
   ]);
+  /**
+   *  @ngdoc directive
+   *  @name ui.grid.paging.directive:uiGridPaging
+   *  @element div
+   *  @restrict A
+   *
+   *  @description Adds paging features to grid
+   *  @example
+   <example module="app">
+   <file name="app.js">
+   var app = angular.module('app', ['ui.grid', 'ui.grid.paging']);
 
+   app.controller('MainCtrl', ['$scope', function ($scope) {
+      $scope.data = [
+        { name: 'Alex', car: 'Toyota' },
+        { name: 'Sam', car: 'Lexus' },
+        { name: 'Joe', car: 'Dodge' },
+        { name: 'Bob', car: 'Buick' },
+        { name: 'Cindy', car: 'Ford' },
+        { name: 'Brian', car: 'Audi' },
+        { name: 'Malcom', car: 'Mercedes Benz' },
+        { name: 'Dave', car: 'Ford' },
+        { name: 'Stacey', car: 'Audi' },
+        { name: 'Amy', car: 'Acura' },
+        { name: 'Scott', car: 'Toyota' },
+        { name: 'Ryan', car: 'BMW' },
+      ];
+
+      $scope.gridOptions = {
+        data: 'data',
+        pagingPageSizes: [5, 10, 25],
+        pagingPageSize: 5,
+        columnDefs: [
+          {name: 'name'},
+          {name: 'car'}
+        ];
+       }
+    }]);
+   </file>
+   <file name="index.html">
+   <div ng-controller="MainCtrl">
+   <div ui-grid="gridOptions" ui-grid-paging></div>
+   </div>
+   </file>
+   </example>
+   */
   module.directive('uiGridPaging', ['gridUtil', 'uiGridPagingService', 
     function (gridUtil, uiGridPagingService) {
       var defaultTemplate = 'ui-grid/ui-grid-paging';
@@ -103,6 +228,13 @@
     }
   ]);
 
+  /**
+   *  @ngdoc directive
+   *  @name ui.grid.paging.directive:uiGridPager
+   *  @element div
+   *
+   *  @description Panel for handling paging
+   */
   module.directive('uiGridPager', ['uiGridPagingService', 'uiGridConstants', 
     function (uiGridPagingService, uiGridConstants) {
       return {
