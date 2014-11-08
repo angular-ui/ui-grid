d5d826b7d2f4ec9f8a80c2656f90602ae5259ba6
KabraKosmica <srosasn@gmail.com>
2014-11-08 21:50:30 +0100

    Update es.js

--- src/js/i18n/es.js
+++ src/js/i18n/es.js
@@ -12,7 +12,7 @@
           placeholder: 'Buscar...',
           showingItems: 'Artículos Mostrados:',
           selectedItems: 'Artículos Seleccionados:',
-          totalItems: 'Total Artículos:',
+          totalItems: 'Artículos Totales:',
           size: 'Tamaño de Página:',
           first: 'Primera Página',
           next: 'Página Siguiente',
@@ -31,7 +31,7 @@
           hide: 'Ocultar la columna'
         },
         aggregation: {
-          count: 'total filas: ',
+          count: 'filas totales: ',
           sum: 'total: ',
           avg: 'media: ',
           min: 'min: ',
@@ -53,11 +53,11 @@
           exporterSelectedAsPdf: 'Exportar selección como pdf'
         },
         importer: {
-          noHeaders: 'No fue posible derivar nombres de columnas, ¿tiene encabezados el archivo?',
-          noObjects: 'No fue posible obtener registros, ¿existe información aparte de los encabezados?',
+          noHeaders: 'No fue posible derivar los nombres de las columnas, ¿tiene encabezados el archivo?',
+          noObjects: 'No fue posible obtener registros, ¿contiene datos el archivo, aparte de los encabezados?',
           invalidCsv: 'No fue posible procesar el archivo, ¿es un CSV válido?',
           invalidJson: 'No fue posible procesar el archivo, ¿es un Json válido?',
-          jsonNotArray: 'Archivo json importado debe contener un arreglo, abortando.'
+          jsonNotArray: 'El archivo json importado debe contener un array, abortando.'
         }
       });
       return $delegate;
