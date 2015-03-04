36358f778bb60c349641f74e4fc4bc1978a5e757
Paul Lambert <paul@planar.id.au>
2015-03-04 19:45:31 +1300

    Fix (ngTemplates): was causing httpbackend exception

--- grunt/ngtemplates.js
+++ grunt/ngtemplates.js
@@ -1,23 +1,21 @@
 module.exports = {
-  ngtemplates: {
-    'uigrid': {
-      // Look for templates in src and in feature template directories
-      src: ['src/templates/**/*.html', 'src/features/*/templates/**/*.html'],
-      dest: '.tmp/template.js',
-      options: {
-        module: 'ui.grid',
-        htmlmin:  { collapseWhitespace: true, collapseBooleanAttributes: true },
-        // Strip .html extension
-        url: function(url) {
-          // Remove the src/templates/ prefix
-          url = url.replace(/^src\/templates\//, '');
+  'uigrid': {
+    // Look for templates in src and in feature template directories
+    src: ['src/templates/**/*.html', 'src/features/*/templates/**/*.html'],
+    dest: '.tmp/template.js',
+    options: {
+      module: 'ui.grid',
+      htmlmin:  { collapseWhitespace: true, collapseBooleanAttributes: true },
+      // Strip .html extension
+      url: function(url) {
+        // Remove the src/templates/ prefix
+        url = url.replace(/^src\/templates\//, '');
 
-          // Replace feature prefix with just 'ui-grid'
-          url = url.replace(/^src\/features\/[^\/]+?\/templates/, 'ui-grid');
+        // Replace feature prefix with just 'ui-grid'
+        url = url.replace(/^src\/features\/[^\/]+?\/templates/, 'ui-grid');
 
-          // Remove the .html extension
-          return url.replace('.html', '');
-        }
+        // Remove the .html extension
+        return url.replace('.html', '');
       }
     }
   }
