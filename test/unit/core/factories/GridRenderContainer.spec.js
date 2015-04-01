f694729e007c650205fa19cfc6ee477c6575e0ee
Paul Lambert <paul@planar.id.au>
2015-04-01 16:51:48 +1300

    Fix(renderContainer): widths can be crept down due to max width

--- test/unit/core/factories/GridRenderContainer.spec.js
+++ test/unit/core/factories/GridRenderContainer.spec.js
@@ -16,11 +16,15 @@ describe('GridRenderContainer factory', function () {
     GridRenderContainer = _GridRenderContainer_;
     uiGridConstants = _uiGridConstants_;
 
-    cols = [
-      { field: 'firstName' }
+    grid = new Grid({ id: 1 });
+
+    grid.options.columnDefs = [
+      { field: 'firstName' },
+      { field: 'lastName' },
+      { field: 'company' },
+      { field: 'gender' }
     ];
 
-    grid = new Grid({ id: 1 });
   }));
 
   describe('constructor', function () {
@@ -94,5 +98,126 @@ describe('GridRenderContainer factory', function () {
   });
 
 
+  describe('updateWidths', function() {
+    beforeEach(function() {
+      grid.buildColumns();
+      grid.setVisibleColumns(grid.columns);
+      spyOn(grid, 'getViewportWidth').andCallFake(function() { return 415;});  // actual width 400 after scrollbar
+    });
+    
+    it('all percentages', function() {
+      grid.columns[0].width = '25%';
+      grid.columns[1].width = '25%';
+      grid.columns[2].width = '25%';
+      grid.columns[3].width = '25%';
+      
+      grid.renderContainers.body.updateColumnWidths();
+      
+      expect( grid.columns[0].drawnWidth ).toEqual(100);
+      expect( grid.columns[1].drawnWidth ).toEqual(100);
+      expect( grid.columns[2].drawnWidth ).toEqual(100);
+      expect( grid.columns[3].drawnWidth ).toEqual(100);
+    });
+
+    it('all percentages, less than 100%', function() {
+      grid.columns[0].width = '20%';
+      grid.columns[1].width = '15%';
+      grid.columns[2].width = '20%';
+      grid.columns[3].width = '15%';
+      
+      grid.renderContainers.body.updateColumnWidths();
+      
+      expect( grid.columns[0].drawnWidth ).toEqual(80);
+      expect( grid.columns[1].drawnWidth ).toEqual(60);
+      expect( grid.columns[2].drawnWidth ).toEqual(80);
+      expect( grid.columns[3].drawnWidth ).toEqual(60);
+    });
+
+    it('all percentages, more than 100%', function() {
+      grid.columns[0].width = '40%';
+      grid.columns[1].width = '30%';
+      grid.columns[2].width = '40%';
+      grid.columns[3].width = '30%';
+      
+      grid.renderContainers.body.updateColumnWidths();
+      
+      expect( grid.columns[0].drawnWidth ).toEqual(160);
+      expect( grid.columns[1].drawnWidth ).toEqual(120);
+      expect( grid.columns[2].drawnWidth ).toEqual(160);
+      expect( grid.columns[3].drawnWidth ).toEqual(120);
+    });
+
+    it('fixed widths', function() {
+      grid.columns[0].width = 50;
+      grid.columns[1].width = 150;
+      grid.columns[2].width = 50;
+      grid.columns[3].width = 150;
+      
+      grid.renderContainers.body.updateColumnWidths();
+      
+      expect( grid.columns[0].drawnWidth ).toEqual(50);
+      expect( grid.columns[1].drawnWidth ).toEqual(150);
+      expect( grid.columns[2].drawnWidth ).toEqual(50);
+      expect( grid.columns[3].drawnWidth ).toEqual(150);
+    });
+
+    it('asterixes', function() {
+      grid.columns[0].width = '*';
+      grid.columns[1].width = '*';
+      grid.columns[2].width = '*';
+      grid.columns[3].width = '*';
+      
+      grid.renderContainers.body.updateColumnWidths();
+      
+      expect( grid.columns[0].drawnWidth ).toEqual(100);
+      expect( grid.columns[1].drawnWidth ).toEqual(100);
+      expect( grid.columns[2].drawnWidth ).toEqual(100);
+      expect( grid.columns[3].drawnWidth ).toEqual(100);
+    });
+
+    it('double asterixes', function() {
+      grid.columns[0].width = '***';
+      grid.columns[1].width = '*';
+      grid.columns[2].width = '***';
+      grid.columns[3].width = '*';
+      
+      grid.renderContainers.body.updateColumnWidths();
+      
+      expect( grid.columns[0].drawnWidth ).toEqual(150);
+      expect( grid.columns[1].drawnWidth ).toEqual(50);
+      expect( grid.columns[2].drawnWidth ).toEqual(150);
+      expect( grid.columns[3].drawnWidth ).toEqual(50);
+    });
+
+    it('asterixes, min width', function() {
+      grid.columns[0].width = '*';
+      grid.columns[1].width = '*';
+      grid.columns[2].width = '*';
+      grid.columns[3].width = '*';
+      grid.columns[0].minWidth = 130;
+      
+      grid.renderContainers.body.updateColumnWidths();
+      
+      expect( grid.columns[0].drawnWidth ).toEqual(130);
+      expect( grid.columns[1].drawnWidth ).toEqual(90);
+      expect( grid.columns[2].drawnWidth ).toEqual(90);
+      expect( grid.columns[3].drawnWidth ).toEqual(90);
+    });
+
+    it('asterixes, max widths', function() {
+      grid.columns[0].width = '*';
+      grid.columns[1].width = '*';
+      grid.columns[2].width = '*';
+      grid.columns[3].width = '*';
+      grid.columns[0].maxWidth = 70;
+      
+      grid.renderContainers.body.updateColumnWidths();
+      
+      expect( grid.columns[0].drawnWidth ).toEqual(70);
+      expect( grid.columns[1].drawnWidth ).toEqual(110);
+      expect( grid.columns[2].drawnWidth ).toEqual(110);
+      expect( grid.columns[3].drawnWidth ).toEqual(110);
+    });
+  });
 
 });
