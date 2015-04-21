4ffaaf26774bae7f52bf4956f45243f6c7dd53a3
Jonathan Stevens <jonathanchris@gmail.com>
2015-04-20 22:01:43 -0400

    fix(pinning): restore correct width state
    closes: #3324

--- src/features/pinning/js/pinning.js
+++ src/features/pinning/js/pinning.js
@@ -205,7 +205,6 @@
         }
         else {
           col.renderContainer = container;
-          col.width = col.drawnWidth;
           if (container === uiGridPinningConstants.container.LEFT) {
             grid.createLeftContainer();
           }
