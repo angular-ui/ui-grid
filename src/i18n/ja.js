57ff7383e9aca326b6038cb81141a2462d870ba0
biblichor <biblichor@yahoo.com>
2015-06-13 17:16:03 +0900

    add i18n-ja

---
+++ src/i18n/ja.js
@@ -0,0 +1,14 @@
+window.ngGrid.i18n['ja'] = {
+    ngAggregateLabel: '項目',
+    ngGroupPanelDescription: '列のヘッダーをグルーピングしたい場所までドラッグアンドドロップしてください。',
+    ngSearchPlaceHolder: '検索',
+    ngMenuText: '列を選択：',
+    ngShowingItemsLabel: '項目を表示：',
+    ngTotalItemsLabel: '全項目：',
+    ngSelectedItemsLabel: '選択された項目：',
+    ngPageSizeLabel: '表示件数：',
+    ngPagerFirstTitle: '最初に戻る',
+    ngPagerNextTitle: '次に進む',
+    ngPagerPrevTitle: '前に戻る',
+    ngPagerLastTitle: '最後に進む'
+};
