6c4444a88e88f2ba6394ed9b54e302162521826a
Jonathan Leitschuh <JLLeitschuh@wpi.edu>
2015-10-02 16:08:01 -0400

    chore(e2e): Fixes the e2e tests
    All e2e tests now pass

--- test/e2e/gridTestUtils.spec.js
+++ test/e2e/gridTestUtils.spec.js
@@ -192,7 +192,8 @@ module.exports = {
     var headerColumns = this.getGrid(gridId)
       .element(by.css('.ui-grid-render-container-body'))
       .element( by.css('.ui-grid-header'))
-      .all(by.repeater('col in colContainer.renderedColumns track by col.uid'));
+      .all(by.repeater('col in colContainer.renderedColumns track by col.uid'))
+      .all(by.css('.ui-grid-header-cell-label'));
 
     expect(headerColumns.count()).toBe(expectedColumns.length);
 
@@ -327,7 +328,8 @@ module.exports = {
   */
   expectHeaderCellValueMatch: function( gridId, expectedCol, expectedValue ) {
     var headerCell = this.headerCell( gridId, expectedCol);
-    expect(headerCell.getText()).toMatch(expectedValue);
+    var headerCellValue = headerCell.element(by.css('.ui-grid-header-cell-label')).getText();
+    expect(headerCellValue).toMatch(expectedValue);
   },
 
   /**
@@ -811,7 +813,8 @@ module.exports = {
     // NOTE: Can't do .click() as it doesn't work when webdriving Firefox
     return browser.actions().mouseMove(gridMenuButton).mouseDown(gridMenuButton).mouseUp().perform()
       .then(function () {
-        var row = gridMenuButton.element( by.repeater('item in menuItems').row( itemNumber) );
+        var row = gridMenuButton.element( by.repeater('item in menuItems').row( itemNumber) )
+                    .element(by.css('.ui-grid-menu-item'));
 
         return browser.actions().mouseMove(row).mouseDown(row).mouseUp().perform();
       });
