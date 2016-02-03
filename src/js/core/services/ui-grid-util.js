8397bc28b12ddad940d1ca7759b63692aba83df7
Vincent Boef <Vincent Boef>
2016-02-03 11:35:38 +0100

    ISSUE: 3782 (https://github.com/angular-ui/ui-grid/issues/3782)
    e can be null in Firefox. This gives the styles is null error.
    
    Add this extra !== null check and the error is gone.

--- src/js/core/services/ui-grid-util.js
+++ src/js/core/services/ui-grid-util.js
@@ -910,7 +910,7 @@ module.service('gridUtil', ['$log', '$window', '$document', '$http', '$templateC
         e = elem[0];
       }
 
-      if (e) {
+      if (e && e !== null) {
         var styles = getStyles(e);
         return e.offsetWidth === 0 && rdisplayswap.test(styles.display) ?
                   s.swap(e, cssShow, function() {
