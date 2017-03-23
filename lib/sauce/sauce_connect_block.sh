177e096eb6cf2b1c659914719508df7f8234f6d1
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2017-03-22 22:44:57 -0400

    chore(logs): Change cat to tail

--- lib/sauce/sauce_connect_block.sh
+++ lib/sauce/sauce_connect_block.sh
@@ -3,10 +3,12 @@
 if [ $TRAVIS_PULL_REQUEST == "false" ]
 then
   # Print connect log to let us know what is going on
+  echo Sauce Labs Log
+  tail $CONNECT_LOG
   echo Sauce Labs Error Log
-  cat $CONNECT_STDERR
+  tail $CONNECT_STDERR
   echo Sauce Labs Connect Log
-  cat $CONNECT_STDOUT
+  tail $CONNECT_STDOUT
   # Wait for Connect to be ready before exiting
   while [ ! -f $SAUCE_CONNECT_READY_FILE ]; do
     sleep .5
