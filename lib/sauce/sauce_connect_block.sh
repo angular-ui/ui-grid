01b5f3e2a026208f864215bd9529875d4658074b
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2017-03-22 22:00:45 -0400

    chore(sauce_connect_block.sh): Priting the connect log while sauce labs is not connected.

--- lib/sauce/sauce_connect_block.sh
+++ lib/sauce/sauce_connect_block.sh
@@ -2,6 +2,8 @@
 
 if [ $TRAVIS_PULL_REQUEST == "false" ]
 then
+  # Print connect log to let us know what is going on
+  cat $CONNECT_LOG
   # Wait for Connect to be ready before exiting
   while [ ! -f $SAUCE_CONNECT_READY_FILE ]; do
     sleep .5
