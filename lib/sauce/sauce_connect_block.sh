54491e8f5cc8c53d907d3fb6a98ae69667dfbe25
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2017-03-22 22:28:07 -0400

    chore(logs): Improving sauce labs logging

--- lib/sauce/sauce_connect_block.sh
+++ lib/sauce/sauce_connect_block.sh
@@ -3,7 +3,10 @@
 if [ $TRAVIS_PULL_REQUEST == "false" ]
 then
   # Print connect log to let us know what is going on
-  cat $CONNECT_LOG
+  echo Sauce Labs Error Log
+  cat $CONNECT_STDERR
+  echo Sauce Labs Connect Log
+  cat $CONNECT_STDOUT
   # Wait for Connect to be ready before exiting
   while [ ! -f $SAUCE_CONNECT_READY_FILE ]; do
     sleep .5
