ac8758cdd407e7fa2f0380d89ca886cdafa34678
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2018-03-12 23:30:18 -0400

    feat(uglify/concat): Update UI-Grid build system to publish multiple files separated by features.
    UI-Grid will now publish a base file with just the basic grid and multiple files for each individual
    feature.
    
    fix #5802

--- grunt/concat.js
+++ grunt/concat.js
@@ -1,10 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const getDirectories = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory());
+
+function getFiles() {
+	const files = {
+		'<%= dist %>/release/<%= pkg.name %>.js': ['src/js/core/bootstrap.js', 'src/js/**/*.js', 'src/features/*/js/**/*.js', '.tmp/template.js'],
+		'<%= dist %>/release/<%= pkg.name %>.base.js': ['src/js/core/bootstrap.js', 'src/js/**/*.js', '.tmp/template.js']
+	};
+	const features = getDirectories('src/features/');
+
+	features.forEach((feat) => {
+		files[`<%= dist %>/release/<%= pkg.name %>.${feat}.js`] = [`src/features/${feat}/js/**/*.js`]
+	});
+
+	return files;
+}
+
 module.exports = {
-  options: {
-    banner: '<%= banner %>',
-    stripBanners: true
-  },
-  dist: {
-    src: ['src/js/core/bootstrap.js', 'src/js/**/*.js', 'src/features/*/js/**/*.js', '.tmp/template.js'],
-    dest: '<%= dist %>/release/<%= pkg.name %>.js'
-  },
+	options: {
+		banner: '<%= banner %>',
+		stripBanners: true
+	},
+	dist: {
+		files: getFiles()
+	}
 };
--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,9 +1,21 @@
+const concat = require('./concat');
+
+function getFiles() {
+	const files = {};
+	const sources = Object.keys(concat.dist.files);
+
+	sources.forEach((srcFile) => {
+		files[srcFile.replace('.js', '.min.js')] = srcFile;
+	});
+
+	return files;
+}
+
 module.exports = {
   options: {
     banner: '<%= banner %>'
   },
   concat: {
-    src: '<%= concat.dist.dest %>',
-    dest: '<%= dist %>/release/<%= pkg.name %>.min.js'
+  	files: getFiles()
   }
 };
