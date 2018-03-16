b96100c28bfe17be502258a303c35e387b51488c
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2018-03-15 21:39:08 -0400

    feat(i18n): Separate language files from ui-grid.base.js
    Improving load performance of ui-grid.base.js by separating out the language files(with the
    exception of english, as that is the default language of the grid).

--- grunt/concat.js
+++ grunt/concat.js
@@ -2,16 +2,27 @@ const fs = require('fs');
 const path = require('path');
 
 const getDirectories = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory());
+const getLanguages = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isFile());
 
 function getFiles() {
 	const files = {
 		'<%= dist %>/release/<%= pkg.name %>.js': ['src/js/core/bootstrap.js', 'src/js/**/*.js', 'src/features/*/js/**/*.js', '.tmp/template.js'],
-		'<%= dist %>/release/<%= pkg.name %>.base.js': ['src/js/core/bootstrap.js', 'src/js/**/*.js', '.tmp/template.js']
+		'<%= dist %>/release/<%= pkg.name %>.base.js': ['src/js/core/bootstrap.js', 'src/js/core/**/*.js', 'src/js/i18n/ui-18n.js',
+			'src/js/i18n/en.js', '.tmp/template.js']
 	};
 	const features = getDirectories('src/features/');
 
 	features.forEach((feat) => {
-		files[`<%= dist %>/release/<%= pkg.name %>.${feat}.js`] = [`src/features/${feat}/js/**/*.js`]
+		files[`<%= dist %>/release/<%= pkg.name %>.${feat}.js`] = [`src/features/${feat}/js/**/*.js`];
+	});
+
+	const languages = getLanguages('src/js/i18n/')
+		.filter((lang) => lang !== 'en.js' && lang !== 'ui-i18n.js');
+
+	files['<%= dist %>/release/i18n/<%= pkg.name %>.language.all.js'] = languages.map((lang) => `src/js/i18n/${lang}`);
+
+	languages.forEach((lang) => {
+		files[`<%= dist %>/release/i18n/<%= pkg.name %>.language.${lang}`] = [`src/js/i18n/${lang}`];
 	});
 
 	return files;
