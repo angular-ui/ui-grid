d49875d3a410015221ca326792ec4b66fe1bfbb1
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2018-04-30 14:45:36 -0400

    chore(bump.js): Ensure package-lock.js file is also updated on version bumps.

--- grunt/bump.js
+++ grunt/bump.js
@@ -1,7 +1,7 @@
 module.exports = {
   options: {
-    files: ['package.json', 'bower.json'],
-    commitFiles: ['package.json', 'bower.json', 'CHANGELOG.md'],
+    files: ['package.json', 'package-lock.json', 'bower.json'],
+    commitFiles: ['package.json', 'package-lock.json', 'bower.json', 'CHANGELOG.md'],
     push: false,
     commitMessage: 'chore: Release v%VERSION%'
   }
