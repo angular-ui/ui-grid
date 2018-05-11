5010ff032f3bd9e013a7d804ae3244be7eb13b0a
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2018-05-10 22:19:30 -0400

    fix(ui-grid-util.js): Replace angular.uppercase and .lowercase with String.toUpperCase and .toLowerC
    This will allow us to support angular 1.7 when it becomes available.
    
    fix #6715

--- src/js/core/services/ui-grid-util.js
+++ src/js/core/services/ui-grid-util.js
@@ -243,11 +243,11 @@ module.service('gridUtil', ['$log', '$window', '$document', '$http', '$templateC
       return columnName.replace(/_+/g, ' ')
         // Replace a completely all-capsed word with a first-letter-capitalized version
         .replace(/^[A-Z]+$/, function (match) {
-          return angular.lowercase(angular.uppercase(match.charAt(0)) + match.slice(1));
+          return match.toLowerCase();
         })
         // Capitalize the first letter of words
         .replace(/([\w\u00C0-\u017F]+)/g, function (match) {
-          return angular.uppercase(match.charAt(0)) + match.slice(1);
+          return match.charAt(0).toUpperCase() + match.slice(1);
         })
         // Put a space in between words that have partial capilizations (i.e. 'firstName' becomes 'First Name')
         // .replace(/([A-Z]|[A-Z]\w+)([A-Z])/g, "$1 $2");
