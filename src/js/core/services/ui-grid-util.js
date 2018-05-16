b032df912d6a49de1b81c5bc7ae5611bb7e4e8d1
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2018-05-16 19:23:15 -0400

    fix(ui-grid-util.js): Replace angular.uppercase with toUpperCase.
    fix #6729

--- src/js/core/services/ui-grid-util.js
+++ src/js/core/services/ui-grid-util.js
@@ -900,7 +900,8 @@ module.service('gridUtil', ['$log', '$window', '$document', '$http', '$templateC
 
 
   ['width', 'height'].forEach(function (name) {
-    var capsName = angular.uppercase(name.charAt(0)) + name.substr(1);
+    var capsName = name.charAt(0).toUpperCase() + name.substr(1);
+
     s['element' + capsName] = function (elem, extra) {
       var e = elem;
       if (e && typeof(e.length) !== 'undefined' && e.length) {
@@ -910,12 +911,11 @@ module.service('gridUtil', ['$log', '$window', '$document', '$http', '$templateC
       if (e && e !== null) {
         var styles = getStyles(e);
         return e.offsetWidth === 0 && rdisplayswap.test(styles.display) ?
-                  s.swap(e, cssShow, function() {
-                    return getWidthOrHeight(e, name, extra );
-                  }) :
-                  getWidthOrHeight( e, name, extra );
-      }
-      else {
+          s.swap(e, cssShow, function() {
+            return getWidthOrHeight(e, name, extra );
+          }) :
+          getWidthOrHeight( e, name, extra );
+      } else {
         return null;
       }
     };
