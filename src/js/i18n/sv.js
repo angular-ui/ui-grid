6dfbad39cad29209b41e1b430edb7e040406d0da
Joakim Brännström <tok_jocke@hotmail.com>
2018-11-05 19:31:39 +0100

    feat(sv.js): Added missing Swedish translations
    Added missing Swedish translations

--- src/js/i18n/sv.js
+++ src/js/i18n/sv.js
@@ -5,6 +5,16 @@
   angular.module('ui.grid').config(['$provide', function($provide) {
     $provide.decorator('i18nService', ['$delegate', function($delegate) {
       $delegate.add('sv', {
+        headerCell: {
+          aria: {
+            defaultFilterLabel: 'Kolumnfilter',
+            removeFilter: 'Ta bort filter',
+            columnMenuButtonLabel: 'Kolumnmeny',
+            column: 'Kolumn'
+          },
+          priority: 'Prioritet:',
+          filterLabel: "Filter för kolumn: "
+        },
         aggregate: {
           label: 'Artiklar'
         },
@@ -12,6 +22,10 @@
           description: 'Dra en kolumnrubrik hit och släpp den för att gruppera efter den kolumnen.'
         },
         search: {
+          aria: {
+            selected: 'Rad är vald',
+            notSelected: 'Rad är inte vald'
+          },
           placeholder: 'Sök...',
           showingItems: 'Visar artiklar:',
           selectedItems: 'Valda artiklar:',
@@ -28,6 +42,7 @@
         sort: {
           ascending: 'Sortera stigande',
           descending: 'Sortera fallande',
+          none: 'Ingen sortering',
           remove: 'Inaktivera sortering'
         },
         column: {
@@ -45,7 +60,13 @@
           pinRight: 'Fäst höger',
           unpin: 'Lösgör'
         },
+        columnMenu: {
+          close: 'Stäng'
+        },
         gridMenu: {
+          aria: {
+              buttonLabel: 'Meny'
+          },
           columns: 'Kolumner:',
           importerTitle: 'Importera fil',
           exporterAllAsCsv: 'Exportera all data som CSV',
@@ -54,6 +75,9 @@
           exporterAllAsPdf: 'Exportera all data som PDF',
           exporterVisibleAsPdf: 'Exportera synlig data som PDF',
           exporterSelectedAsPdf: 'Exportera markerad data som PDF',
+          exporterAllAsExcel: 'Exportera all data till Excel',
+          exporterVisibleAsExcel: 'Exportera synlig data till Excel',
+          exporterSelectedAsExcel: 'Exportera markerad data till Excel',
           clearAllFilters: 'Rengör alla filter'
         },
         importer: {
@@ -64,8 +88,33 @@
           jsonNotArray: 'Importerad JSON-fil måste innehålla ett fält. Import avbruten.'
         },
         pagination: {
+          aria: {
+            pageToFirst: 'Gå till första sidan',
+            pageBack: 'Gå en sida bakåt',
+            pageSelected: 'Vald sida',
+            pageForward: 'Gå en sida framåt',
+            pageToLast: 'Gå till sista sidan'
+          },
           sizes: 'Artiklar per sida',
-          totalItems: 'Artiklar'
+          totalItems: 'Artiklar',
+          through: 'genom',
+          of: 'av'
+        },
+        grouping: {
+          group: 'Gruppera',
+          ungroup: 'Dela upp',
+          aggregate_count: 'Agg: Antal',
+          aggregate_sum: 'Agg: Summa',
+          aggregate_max: 'Agg: Max',
+          aggregate_min: 'Agg: Min',
+          aggregate_avg: 'Agg: Genomsnitt',
+          aggregate_remove: 'Agg: Ta bort'
+        },
+        validate: {
+          error: 'Error:',
+          minLength: 'Värdet borde vara minst THRESHOLD tecken långt.',
+          maxLength: 'Värdet borde vara max THRESHOLD tecken långt.',
+          required: 'Ett värde krävs.'
         }
       });
       return $delegate;
