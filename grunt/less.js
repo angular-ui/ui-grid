1bcedf067fc228f80ef4831e19445175d40c2258
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2018-12-26 15:17:16 -0500

    chore(packages): add build steps for scoped packages

--- grunt/less.js
+++ grunt/less.js
@@ -1,32 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+
+const getDirectories = p => fs.readdirSync(p)
+.filter(f => fs.statSync(path.join(p, f)).isDirectory() && fs.existsSync(path.join(p, f, 'less')));
+
+function filterCoreLessFiles(filepath) {
+  return filepath === 'packages/core/less/main.less' || !filepath.includes('packages/core/less/');
+}
+
+function getFiles(compress) {
+  const suffix = compress ? 'min.css' : 'css';
+	const files = [
+    {
+      src: ['packages/core/less/main.less', 'packages/*/less/**/*.less', '.tmp/font/ui-grid-codes.css'],
+      dest: `dist/release/<%= pkg.name %>.${suffix}`,
+      filter: filterCoreLessFiles
+    }
+  ];
+	const packages = getDirectories('packages/');
+
+	packages.forEach((feat) => {
+    files.push({
+      src: `packages/${feat}/less/*.less`,
+      dest: `packages/${feat}/<%= pkg.name %>.${feat}.${suffix}`,
+      filter: filterCoreLessFiles
+    });
+  });
+
+	return files;
+}
+
 module.exports = {
   dist: {
-    // paths: ['/bower_components/bootstrap'],
     options: {
       banner: '<%= banner %>'
     },
-    files: [
-      {
-        src: ['packages/core/less/main.less', 'packages/*/less/**/*.less', '.tmp/font/ui-grid-codes.css'],
-        dest: 'dist/release/<%= pkg.name %>.css',
-        filter: function(filepath) {
-          return filepath === 'packages/core/less/main.less' || !filepath.includes('packages/core/less/');
-        }
-      }
-    ]
+    files: getFiles()
   },
   min: {
     options: {
       banner: '<%= banner %>',
       compress: true
     },
-    files: [
-      {
-        src: ['packages/core/less/main.less', 'packages/*/less/**/*.less', '.tmp/font/ui-grid-codes.css'],
-        dest: 'dist/release/<%= pkg.name %>.min.css',
-        filter: function(filepath) {
-          return filepath === 'packages/core/less/main.less' || !filepath.includes('packages/core/less/');
-        }
-      }
-    ]
+    files: getFiles(true)
   }
 };
