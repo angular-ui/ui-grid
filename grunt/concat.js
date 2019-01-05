9c9961812f764e64dbd2feae248c46b54a063a5e
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2019-01-05 14:10:07 -0500

    chore(template): only load templates needed for each feature
    Updated the individual files to ensure they each only load the templates
    that they require.

--- grunt/concat.js
+++ grunt/concat.js
@@ -3,11 +3,14 @@ const path = require('path');
 
 const getDirectories = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory());
 const getLanguages = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isFile());
+const getTemplateDirectories = p => fs.readdirSync(p)
+.filter(f => fs.statSync(path.join(p, f)).isDirectory() && fs.existsSync(path.join(p, f, 'src/templates')));
+const templateDirectories = getTemplateDirectories('packages/');
 
 function getFiles() {
 	const files = {
 		'<%= dist %>/release/<%= pkg.name %>.js': ['packages/core/src/js/bootstrap.js', 'packages/**/src/js/**/*.js', '.tmp/template.js'],
-		'packages/core/<%= pkg.name %>.core.js': ['src/js/core/bootstrap.js', 'packages/core/src/js/**/*.js', '.tmp/template.js']
+		'packages/core/<%= pkg.name %>.core.js': ['src/js/core/bootstrap.js', 'packages/core/src/js/**/*.js', '.tmp/template-core.js']
 	};
 	const packages = getDirectories('packages/');
 
@@ -21,7 +24,13 @@ function getFiles() {
 				files[`packages/i18n/<%= pkg.name %>.language.${lang}`] = [`packages/i18n/src/js/${lang}`];
 			});
 		} else if (feat !== 'core') {
-			files[`packages/${feat}/<%= pkg.name %>.${feat}.js`] = [`packages/${feat}/src/js/**/*.js`];
+			let src = [`packages/${feat}/src/js/**/*.js`];
+
+			if (templateDirectories.includes(feat)) {
+				src.push(`.tmp/template-${feat}.js`);
+			}
+			
+			files[`packages/${feat}/<%= pkg.name %>.${feat}.js`] = src;
 		}
 	});
 
